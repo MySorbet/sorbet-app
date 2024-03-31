d5751c9cd3409e16104d395141f98a5582c2c02b
Humza Khan <humzakhan@outlook.com>
2024-03-31 04:50:40 -0400

    add notifications dropdown

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,6 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
+import { Notifications } from '@/components/header/notifications';
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '@/components/ui/hover-card';
 import { useAppDispatch } from '@/redux/hook';
 import { setOpenSidebar } from '@/redux/userSlice';
+import { Bell } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 
@@ -15,12 +22,17 @@ export const Header = () => {
           <img src='/svg/logo.svg' alt='logo' width={44} height={44} />
         </div>
         <div className='flex items-center justify-end gap-4'>
-          <img
-            src='/images/menu.svg'
-            alt='menu'
-            className='cursor-pointer p-[10px]'
-            onClick={() => dispatch(setOpenSidebar(true))}
-          />
+          <div className='flex flex-row align-center gap-2 items-center'>
+            <Notifications />
+            <div>
+              <img
+                src='/images/menu.svg'
+                alt='menu'
+                className='cursor-pointer p-[10px]'
+                onClick={() => dispatch(setOpenSidebar(true))}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
