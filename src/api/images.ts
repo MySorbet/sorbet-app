ab6b78e5de4cea9b5b7be1117a10694384b744a2
Humza Khan <humzakhan@outlook.com>
2024-04-11 22:24:02 -0400

    implement profile image upload

--- src/api/images.ts
+++ src/api/images.ts
@@ -1,11 +1,6 @@
 import { API_URL, runApi } from '@/utils';
 
-import { ProfileAvatar } from '@/types';
-
-// FIXME: api is not complete
-// [POST] /api/images/uploadProfileImage
 export const uploadProfileImageAsync = async (data: FormData) => {
-  // const reqBody = {};
-  const res = await runApi('POST', `${API_URL}/images/upload`, data);
+  const res = await runApi('POST', `${API_URL}/images/upload`, data, {}, true);
   return res;
 };
