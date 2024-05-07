ef0f99b45cf0e777076592c3c7f4406afe412c64
Humza Khan <humzakhan@outlook.com>
2024-05-06 21:20:36 -0400

    set meta tags for all social platforms

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,31 +1,39 @@
 export default function Head() {
+  const title = 'Sorbet | Ultimate Tool for Creative Freelancers';
+  const description =
+    'The all-in-one solution for your freelance work, powered by blockchain.';
+  const imageUrl =
+    'http://framerusercontent.com/images/oIBwaUw8Is43oxKK9ekodxxA.png';
+
   return (
     <head>
       <title>Sorbet</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
 
-      <meta
-        name='description'
-        content='Join the social network built on the NEAR blockchain that fosters collaboration between creators. Share your work, collaborate with others, and build your audience.'
-      />
+      <meta name='description' content={description} />
       <meta
         name='keywords'
         content='social network, NEAR blockchain, collaboration, creators, ai'
       />
 
-      <meta
-        property='og:title'
-        content='Sorbet | Collaborative Platform for Creators'
-      />
-      <meta
-        property='og:description'
-        content='Join the social network built on the NEAR blockchain that fosters collaboration between creators. Share your work, collaborate with others, and build your audience.'
-      />
-      <meta
-        property='og:image:alt'
-        content='Sorbet - The Web3 Social Marketplace for Creatives.'
-      />
+      {/* Open Graph / Facebook */}
       <meta property='og:type' content='website' />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:image' content={imageUrl} />
+      <meta property='og:image:alt' content={description} />
+
+      {/* Twitter */}
+      <meta property='twitter:card' content='summary_large_image' />
+      <meta property='twitter:title' content={title} />
+      <meta property='twitter:description' content={description} />
+      <meta property='twitter:image' content={imageUrl} />
+
+      {/* LinkedIn */}
+      <meta property='linkedin:title' content={title} />
+      <meta property='linkedin:description' content={description} />
+      <meta property='linkedin:image' content={imageUrl} />
+
       <link rel='icon' href='/favicon.ico' />
     </head>
   );
