9db889b9e6ce679ae29e9948b172b7907cda60d2
Humza Khan <humzakhan@outlook.com>
2024-05-12 23:38:22 -0400

    remove unused imports

--- src/app/gigs/contract/contract-container.tsx
+++ src/app/gigs/contract/contract-container.tsx
@@ -4,17 +4,6 @@ import {
   ContractMilestones,
   ContractMilestonesFormData,
 } from '@/app/gigs/contract';
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { CircleCheckBig } from 'lucide-react';
 import React, { useState } from 'react';
