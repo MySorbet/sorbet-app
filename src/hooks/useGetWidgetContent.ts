75cac4cc745995b6e7333b7474aa1e4a3fb2bc72
Chris Park <cp3063@columbia.edu>
2024-06-05 13:43:45 -0700

    Adds error callback for useGetWidgetContent hook

--- src/hooks/useGetWidgetContent.ts
+++ src/hooks/useGetWidgetContent.ts
@@ -1,10 +1,16 @@
+import { useToast } from '@/components/ui/use-toast';
 import { getWidgetContent } from '@/lib/service';
 import { WidgetType } from '@/types';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 export const useGetWidgetContent = () => {
+  const { toast } = useToast();
+
   return useMutation({
     mutationFn: ({ url, type }: { url: string; type: WidgetType }) =>
       getWidgetContent({ url, type }),
+    onError: (error) => {
+      toast({ title: 'Error', description: 'Failed to fetch widget content' });
+    },
   });
 };
