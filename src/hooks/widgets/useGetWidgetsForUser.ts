b5f1024bae011f4f45cf7fb025cadb5032aa6288
Chris Park <cp3063@columbia.edu>
2024-06-07 17:42:57 -0700

    Adds userId to queryKey array for useGetWidgetsForUser hook

--- src/hooks/widgets/useGetWidgetsForUser.ts
+++ src/hooks/widgets/useGetWidgetsForUser.ts
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 
 export const useGetWidgetsForUser = (userId: string) => {
   return useQuery({
-    queryKey: ['widgets'],
+    queryKey: ['widgets', userId],
     queryFn: async () => await getWidgetsForUser(userId),
   });
 };
