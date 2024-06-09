37a0bdc3bf834cd2955f7662fc8a2ffd82b52ac4
Chris Park <cp3063@columbia.edu>
2024-06-09 15:05:08 -0700

    Minor: fixes spelling error

--- src/hooks/widgets/index.ts
+++ src/hooks/widgets/index.ts
@@ -1,7 +1,7 @@
 import { useDeleteWidget } from './useDeleteWidget';
 import { useGetWidgetContent } from './useGetWidgetContent';
 import { useGetWidgetsForUser } from './useGetWidgetsForUser';
-import { useUpdateWidgetsBulk } from './useUpdateWidgesBulk';
+import { useUpdateWidgetsBulk } from './useUpdateWidgetsBulk';
 import { useUploadWidgetsImage } from './useUploadWidgetsImage';
 
 export {
