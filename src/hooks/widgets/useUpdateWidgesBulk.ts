42856ccfb7707e6c4e76622e6ed699bd48fe6de4
Chris Park <cp3063@columbia.edu>
2024-06-09 14:45:21 -0700

    Implements directly rendering the data received from the query as well as optimistically updating. Just needs to address fetching of images and widget metadata.

--- src/hooks/widgets/useUpdateWidgesBulk.ts
+++ src/hooks/widgets/useUpdateWidgesBulk.ts
@@ -3,12 +3,22 @@ import { updateWidgetsBulk } from '@/lib/service';
 import { UpdateWidgetsBulkDto } from '@/types';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
+type UpdateWidgetsBulkParams = {
+  payload: UpdateWidgetsBulkDto[];
+  userId: string;
+};
+
 export const useUpdateWidgetsBulk = () => {
   const queryClient = useQueryClient();
   const { toast } = useToast();
 
   return useMutation({
-    mutationFn: (payload: UpdateWidgetsBulkDto[]) => updateWidgetsBulk(payload),
+    onMutate: async (data: UpdateWidgetsBulkParams) => {
+      await queryClient.cancelQueries({ queryKey: ['widgets', data.userId] });
+      queryClient.setQueryData(['widgets', data.userId], data.payload);
+    },
+    mutationFn: async (data: UpdateWidgetsBulkParams) =>
+      await updateWidgetsBulk(data.payload),
     onSettled: async () =>
       await queryClient.invalidateQueries({ queryKey: ['widgets'] }),
     onError: (error) =>
