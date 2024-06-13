4b2977344d994c1a5a40e559c4982835c7ebbc22
Chris Park <cp3063@columbia.edu>
2024-06-13 13:10:27 -0700

    Feature: styling for signin container

--- src/components/signin/signin-container.tsx
+++ src/components/signin/signin-container.tsx
@@ -1,7 +1,9 @@
 const SignInContainer = () => {
   return (
-    <div>
-      <h1>Sign In</h1>
+    <div className='flex w-screen h-screen items-center justify-center bg-red-900 bg-gradient-to-r from-[#FFFFFF] to-[#D4CEFD]'>
+      <div className='h-[562px] w-[980px] border border-[#4F38DD] border-opacity-80 rounded-[32px] bg-gradient-to-r from-[#FFFFFFCC] to-[#D4CEFDCC]  p-8 pb-12'>
+        Sign in Component
+      </div>
     </div>
   );
 };
