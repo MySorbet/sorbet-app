22f539ad7942283ffdc3f315be3184feee9cd07d
Chris Park <cp3063@columbia.edu>
2024-06-13 12:57:23 -0700

    Minor: sets up basic structure for files for sign up components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './header';
 export * from './profile';
 export * from './sidebar';
 export * from './sidebar';
+export * from './signin';
---
+++ src/components/signin/index.ts
@@ -0,0 +1,3 @@
+import { SignInContainer } from './signin-container';
+
+export { SignInContainer };
