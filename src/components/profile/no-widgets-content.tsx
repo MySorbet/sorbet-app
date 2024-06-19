f751a80b099e4bb973c70cc4b9090210d4e08812
Chris Park <cp3063@columbia.edu>
2024-06-18 22:34:38 -0700

    Feature: adds new NoWidgetContent display

--- src/components/profile/no-widgets-content.tsx
+++ src/components/profile/no-widgets-content.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const NoWidgetsContent = () => {
   return (
-    <div className='bg-[#FFFFFF] rounded-3xl'>
-      <div>
+    <div className='bg-[#FFFFFF] rounded-3xl flex flex-1 items-center justify-center h-40'>
+      <div className='flex flex-col gap-4 items-center justify-center'>
         <h1 className='text-base font-medium text-[#344054]'>
           Start by adding links from your favorite sites
         </h1>
