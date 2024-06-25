3a00d1e0986274d444b8ee0821060f6841e5e24c
Chris Park <cp3063@columbia.edu>
2024-06-24 22:40:07 -0700

    Refactor: adds formatted response in signInAsync

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,7 @@
 import { currentNetwork } from '@/lib/config';
 import { User } from '@/types';
 import { SignInWithEmailTypes, SignUpWithEmailTypes } from '@/types/auth';
-import { API_URL, runApi } from '@/utils';
+import { API_URL, getFormatedResponse, runApi } from '@/utils';
 import axios from 'axios';
 
 export const signUpAsync = async ({
@@ -33,16 +33,10 @@ export const signUpAsync2 = async ({
 };
 
 export const signInAsync = async ({ email }: SignInWithEmailTypes) => {
-  const reqBody = { email };
-  const res = await runApi('POST', `${API_URL}/auth/signin/email`, reqBody);
-  return res;
-};
-
-export const signInAsync2 = async ({ email }: SignInWithEmailTypes) => {
   try {
     const reqBody = { email };
     const res = await axios.post(`${API_URL}/auth/signin/email`, reqBody);
-    return res.data;
+    return getFormatedResponse(res);
   } catch (error: any) {
     throw new Error(error.message);
   }
