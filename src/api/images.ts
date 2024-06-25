f79e84c97fbaaf2623c522492942f0c8cd001753
Chris Park <cp3063@columbia.edu>
2024-06-24 19:09:04 -0700

    Feature: creates new uploadProfileImageAsync function without runApi

--- src/api/images.ts
+++ src/api/images.ts
@@ -1,8 +1,24 @@
-import { API_URL, runApi } from '@/utils';
+import { API_URL, runApi, validateToken } from '@/utils';
+import axios from 'axios';
 
 export const uploadProfileImageAsync = async (data: FormData) => {
   const res = await runApi('POST', `${API_URL}/images/upload`, data, {}, true);
   return res;
+};
+
+export const uploadProfileImageAsync2 = async (data: FormData) => {
+  const apiReqHeader = validateToken({}, true);
+  try {
+    const response = await axios.post(
+      `${API_URL}/images/upload`,
+      data,
+      apiReqHeader
+    );
+
+    return response;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
 };
 
 export const uploadWidgetsImageAsync = async (data: FormData) => {
