273ad65bdcf0040639487a5cb8670360a6bf4203
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-07-15 17:58:00 -0600

    Update all set copy (#29)
    * Update all set copy
    
    * Use more concise more playful language

--- src/components/signup/all-set.tsx
+++ src/components/signup/all-set.tsx
@@ -13,11 +13,10 @@ const AllSet = () => {
   return (
     <FormContainer>
       <div className='flex flex-col gap-6 h-full'>
-        <h1 className='font-semibold text-2xl'>All set!</h1>
+        <h1 className='font-semibold text-2xl'>Your profile is ready 🎉</h1>
         <div className='flex flex-col bg-[#F0EBF9] h-full py-4 px-3 rounded-xl gap-4'>
           <p className='text-sm font-medium text-[#344054]'>
-            Your profile is ready. Start by adding widgets using the input at
-            the bottom of the viewport.
+            Start by adding widgets using the textbox at the bottom of the page
           </p>
           <PasteLinkDemo />
           <p className='text-sm font-medium text-[#344054]'>
@@ -25,8 +24,8 @@ const AllSet = () => {
           </p>
           <SocialsDemo />
           <p className='text-sm font-medium text-[#344054]'>
-            Once added you can change the size by hovering over a widget and
-            selecting from the control
+            You can change the size by hovering over a widget and selecting a
+            new size
           </p>
           <WidgetResizeDemo />
         </div>
