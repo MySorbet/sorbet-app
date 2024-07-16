ab7c1266cd43787f7f93824f50b9afbf9f8b330a
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-07-16 16:58:34 -0600

    x.com is a valid url

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,3 +1,10 @@
+/**
+ * Check if a given url is valid
+ *
+ * @param url The url to check for validity
+ * @returns whether the url is valid or not
+ * @throws if the hostname is not supported
+ */
 export function validateUrl(url: string) {
   let regex: RegExp;
   const hostname = new URL(url).hostname;
@@ -14,15 +21,17 @@ export function validateUrl(url: string) {
   } else if (hostname.includes('soundcloud.com')) {
     regex = /^https?:\/\/(www\.)?soundcloud\.com\/[^\/]+\/[^\/]+$/;
   } else if (hostname.includes('instagram.com')) {
-    regex = /^(https?:\/\/)?(www\.)?instagram\.com\/(p\/[a-zA-Z0-9-_]+\/?|[^\/]+\/?)$/;
+    regex =
+      /^(https?:\/\/)?(www\.)?instagram\.com\/(p\/[a-zA-Z0-9-_]+\/?|[^\/]+\/?)$/;
   } else if (hostname.includes('substack.com')) {
     regex = /^https?:\/\/([a-zA-Z0-9]+\.substack\.com\/).*$/;
   } else if (hostname.includes('behance.net')) {
     regex = /^https?:\/\/(www\.)?behance\.net\/gallery\/[a-zA-Z0-9]+\/[^\/]+$/;
   } else if (hostname.includes('medium.com')) {
-    regex = /^https?:\/\/(www\.)?medium\.com\/@?[a-zA-Z0-9-]+(\/[a-zA-Z0-9-]+)?$/;
-  } else if (hostname.includes('twitter.com')) {
-    regex = /^(https?:\/\/)?(www\.)?twitter\.com\/[a-zA-Z0-9_]+\/?$/;
+    regex =
+      /^https?:\/\/(www\.)?medium\.com\/@?[a-zA-Z0-9-]+(\/[a-zA-Z0-9-]+)?$/;
+  } else if (hostname.includes('twitter.com') || hostname.includes('x.com')) {
+    regex = /^(https?:\/\/)?(www\.)?(twitter|x)\.com\/[a-zA-Z0-9_]+\/?$/;
   } else {
     throw new Error(`Invalid hostname: ${hostname}`);
   }
