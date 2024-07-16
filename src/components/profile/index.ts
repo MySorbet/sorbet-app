f7a788bd3e29d558991e1f7150350372bcd1054b
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-07-16 15:34:15 -0600

    Update index

--- src/components/profile/index.ts
+++ src/components/profile/index.ts
@@ -1,42 +1,42 @@
-import { Profile } from "./profile";
-import { ProfileHeader } from "./profile-header"; 
-import { Widget } from "./widgets/widget";
-import { WidgetContainer } from "./widgets/widget-container"; 
-import { ResizeWidget } from "./widgets/resize-widget"; 
-import { AddWidgets } from "./widgets/add-widgets"; 
-import { InvalidAlert } from "./invalid-alert"; 
-import { ProfileEditModal } from "./profile-edit-modal";
-import { InputLocation } from "./input-location"; 
-import { InputSkills } from "./input-skills"; 
-import { YouTubeIframe } from "./widgets/youtube-iframe"; 
-import { WidgetHeader } from "./widgets/widget-header"; 
-import { DefaultWidget } from "./widgets/widget-default"; 
-import { DribbbleWidget } from "./widgets/widget-dribbble"; 
-import { BehanceWidget } from "./widgets/widget-behance"; 
-import { MediumWidget } from "./widgets/widget-medium"; 
-import { YouTubeWidget } from "./widgets/widget-youtube"; 
-import { SubstackWidget } from "./widgets/widget-substack"; 
-import { SpotifyAlbumWidget } from "./widgets/widget-spotify-album"; 
-import { SpotifySongWidget } from "./widgets/widget-spotify-song"; 
-import { SoundcloudWidget } from "./widgets/widget-soundcloud"; 
-import { InstagramWidget } from "./widgets/widget-instagram"; 
-import { GithubWidget } from "./widgets/widget-github"; 
-import { FigmaWidget } from "./widgets/widget-figma"; 
-import { TwitterWidget } from "./widgets/widget-twitter"; 
-import { PhotoWidget } from "./widgets/widget-photo"; 
-import { NoWidgetsContent } from "./widgets/no-widgets-content"; 
+import { InputLocation } from './input-location';
+import { InputSkills } from './input-skills';
+import { Profile } from './profile';
+import { ProfileEditModal } from './profile-edit-modal';
+import { ProfileHeader } from './profile-header';
+import { AddWidgets } from './widgets/add-widgets';
+import { InvalidAlert } from './widgets/invalid-alert';
+import { NoWidgetsContent } from './widgets/no-widgets-content';
+import { ResizeWidget } from './widgets/resize-widget';
+import { Widget } from './widgets/widget';
+import { BehanceWidget } from './widgets/widget-behance';
+import { WidgetContainer } from './widgets/widget-container';
+import { DefaultWidget } from './widgets/widget-default';
+import { DribbbleWidget } from './widgets/widget-dribbble';
+import { FigmaWidget } from './widgets/widget-figma';
+import { GithubWidget } from './widgets/widget-github';
+import { WidgetHeader } from './widgets/widget-header';
+import { InstagramWidget } from './widgets/widget-instagram';
+import { MediumWidget } from './widgets/widget-medium';
+import { PhotoWidget } from './widgets/widget-photo';
+import { SoundcloudWidget } from './widgets/widget-soundcloud';
+import { SpotifyAlbumWidget } from './widgets/widget-spotify-album';
+import { SpotifySongWidget } from './widgets/widget-spotify-song';
+import { SubstackWidget } from './widgets/widget-substack';
+import { TwitterWidget } from './widgets/widget-twitter';
+import { YouTubeWidget } from './widgets/widget-youtube';
+import { YouTubeIframe } from './widgets/youtube-iframe';
 
-export { 
-  Profile, 
-  ProfileHeader, 
-  Widget, 
-  WidgetContainer, 
-  ResizeWidget, 
-  AddWidgets, 
-  InvalidAlert, 
-  ProfileEditModal, 
-  InputLocation, 
-  InputSkills, 
+export {
+  Profile,
+  ProfileHeader,
+  Widget,
+  WidgetContainer,
+  ResizeWidget,
+  AddWidgets,
+  InvalidAlert,
+  ProfileEditModal,
+  InputLocation,
+  InputSkills,
   DefaultWidget,
   DribbbleWidget,
   BehanceWidget,
@@ -53,5 +53,5 @@ export {
   FigmaWidget,
   TwitterWidget,
   PhotoWidget,
-  NoWidgetsContent
+  NoWidgetsContent,
 };
