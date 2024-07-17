cee3183ce47b5ac80316ca274f36f8a50f0941b1
Chris Park <cp3063@columbia.edu>
2024-07-17 01:58:41 -0700

    Feature: creates message handler for new message event, and initializes chat for user. Fetches most recent messages for current channel and updates local state with this data. State will be cleaned up b/c not using most of it

--- src/app/gigs/chat/chat-layout-minimal.tsx
+++ src/app/gigs/chat/chat-layout-minimal.tsx
@@ -2,24 +2,113 @@
 
 import { Chat } from './chat';
 import { Message, userData } from './data';
-import React, { useEffect, useState } from 'react';
+import { useAuth } from '@/hooks';
+import { SBMessage } from '@/types/sendbird';
+import {
+  initializeConnection,
+  loadMessages,
+  timestampToTime,
+} from '@/utils/sendbird';
+import { MessageListParams } from '@sendbird/chat/message';
+import React, { useEffect, useRef, useState } from 'react';
 
 interface ChatLayoutProps {
   defaultLayout: number[] | undefined;
   defaultCollapsed?: boolean;
   navCollapsedSize: number;
-  handlewNewMessage?: (newMessage: Message) => void;
+  channelId?: string;
 }
 
 export function ChatLayoutMinimal({
   defaultLayout = [320, 480],
   defaultCollapsed = false,
   navCollapsedSize,
-  handlewNewMessage,
+  channelId,
 }: ChatLayoutProps) {
   const [isCollapsed, setIsCollapsed] = React.useState(defaultCollapsed);
   const [selectedUser, setSelectedUser] = React.useState(userData[0]);
   const [isMobile, setIsMobile] = useState(false);
+  const [state, updateState] = useState({
+    applicationUsers: [],
+    groupChannelMembers: [],
+    currentlyJoinedChannel: null,
+    messages: [],
+    channels: [],
+    messageInputValue: '',
+    userNameInputValue: '',
+    userIdInputValue: '',
+    channelNameUpdateValue: '',
+    settingUpUser: true,
+    file: null,
+    messageToUpdate: null,
+    typingMembers: [],
+    messageCollection: null,
+    loading: false,
+    error: false,
+  });
+  const { user } = useAuth();
+  const stateRef = useRef<any>();
+  stateRef.current = state;
+
+  const messageHandlers = {
+    onMessagesAdded: (context: any, channel: any, messages: any) => {
+      console.log('onMessagesAdded', messages);
+      messages.forEach((currentMessage: any) => {
+        const messageToAdd = {
+          userId: currentMessage.sender.userId,
+          message: currentMessage.message,
+          nickname: currentMessage.sender.nickname,
+          avatar: currentMessage.sender.plainProfileUrl,
+        };
+        const updatedMessages = [...stateRef.current.messages, messageToAdd];
+        updateState({ ...stateRef.current, messages: updatedMessages });
+      });
+    },
+  };
+
+  useEffect(() => {
+    async function initializeChat() {
+      const sbuser = await initializeConnection(user?.id);
+
+      if (channelId) {
+        const { collection, channel } = await loadMessages(
+          channelId,
+          messageHandlers
+        );
+        const ts = Date.now();
+        const messageListParams: MessageListParams = {
+          prevResultSize: 20,
+          nextResultSize: 0,
+          isInclusive: true,
+        };
+
+        const latestMessages = await channel.getMessagesByTimestamp(
+          ts,
+          messageListParams
+        );
+
+        const fetchedMessages = latestMessages.map((currentMessage: any) => {
+          const time = timestampToTime(currentMessage.createdAt);
+          return {
+            userId: currentMessage.sender.userId,
+            message: currentMessage.message,
+            nickname: currentMessage.sender.nickname,
+            avatar: currentMessage.sender.plainProfileUrl,
+            timestampData: time,
+          };
+        });
+
+        updateState({
+          ...stateRef.current,
+          messageCollection: collection,
+          currentlyJoinedChannel: channel,
+          messages: fetchedMessages,
+        });
+      }
+    }
+
+    initializeChat();
+  }, [channelId]);
 
   useEffect(() => {
     const checkScreenWidth = () => {
@@ -40,11 +129,11 @@ export function ChatLayoutMinimal({
 
   return (
     <Chat
-      messages={[]}
-      selectedUser={selectedUser}
-      handlewNewMessage={handlewNewMessage}
+      messages={state.messages}
+      selectedUser={user!}
       isMobile={isMobile}
       showTopbar={false}
+      channel={state.currentlyJoinedChannel}
     />
   );
 }
