d6baed066b2886c6347e5b8f422425e5378c0f17
Chris Park <cp3063@columbia.edu>
2024-07-18 02:31:35 -0700

    Refactor: changes ui to match figma mock up

--- src/app/gigs/chat/chat-list.tsx
+++ src/app/gigs/chat/chat-list.tsx
@@ -3,6 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { cn } from '@/lib/utils';
 import { User } from '@/types';
 import { SBMessage } from '@/types/sendbird';
+import { convertMilitaryToRegular } from '@/utils/sendbird';
 import { AnimatePresence, motion } from 'framer-motion';
 import React, { useRef } from 'react';
 
@@ -35,45 +36,58 @@ export function ChatList({
         className='w-full overflow-y-auto overflow-x-hidden h-full flex flex-col'
       >
         <AnimatePresence>
-          {messages?.map((message, index) => (
-            <motion.div
-              key={index}
-              layout
-              initial={{ opacity: 0, scale: 1, y: 50, x: 0 }}
-              animate={{ opacity: 1, scale: 1, y: 0, x: 0 }}
-              exit={{ opacity: 0, scale: 1, y: 1, x: 0 }}
-              transition={{
-                opacity: { duration: 0.1 },
-                layout: {
-                  type: 'spring',
-                  bounce: 0.3,
-                  duration: messages.indexOf(message) * 0.05 + 0.2,
-                },
-              }}
-              style={{
-                originX: 0.5,
-                originY: 0.5,
-              }}
-              className={cn(
-                'flex flex-col gap-2 p-2 whitespace-pre-wrap',
-                message.userId === selectedUser.id ? 'items-end' : 'items-start'
-              )}
-            >
-              <div className='flex gap-1 items-center'>
-                {message.userId === selectedUser.id && (
-                  <Avatar className='flex justify-center items-center'>
-                    <AvatarImage
-                      src={message.avatar ? message.avatar : '/avatar.svg'}
-                      alt={message.nickname}
-                      width={6}
-                      height={6}
-                    />
-                    <AvatarFallback>{message.nickname[0]}</AvatarFallback>
-                  </Avatar>
+          {messages?.map((message, index) => {
+            const time = convertMilitaryToRegular(
+              message?.timestampData?.hour,
+              message?.timestampData?.minute
+            );
+            return (
+              <motion.div
+                key={index}
+                layout
+                initial={{ opacity: 0, scale: 1, y: 50, x: 0 }}
+                animate={{ opacity: 1, scale: 1, y: 0, x: 0 }}
+                exit={{ opacity: 0, scale: 1, y: 1, x: 0 }}
+                transition={{
+                  opacity: { duration: 0.1 },
+                  layout: {
+                    type: 'spring',
+                    bounce: 0.3,
+                    duration: messages.indexOf(message) * 0.05 + 0.2,
+                  },
+                }}
+                style={{
+                  originX: 0.5,
+                  originY: 0.5,
+                }}
+                className={cn(
+                  'flex flex-col whitespace-pre-wrap',
+                  'items-start'
                 )}
+              >
+                <div className='flex flex-col gap-1 items-center'>
+                  {index === 0 ||
+                    (messages[index - 1].userId !== message.userId && (
+                      <div className='flex items-center gap-1 mt-5'>
+                        <Avatar className='flex justify-center items-center'>
+                          <AvatarImage
+                            src={
+                              message.avatar ? message.avatar : '/avatar.svg'
+                            }
+                            alt={message.nickname}
+                            width={6}
+                            height={6}
+                          />
+                          <AvatarFallback>{message.nickname[0]}</AvatarFallback>
+                        </Avatar>
+                        <span className='text-base'>{message.nickname}</span>
+                        <span className='text-sm'>{time}</span>
+                      </div>
+                    ))}
+                </div>
                 <span
                   className={cn(
-                    'bg-accent p-3 mr-3 rounded-3xl max-w-xs',
+                    'bg-accent p-2 px-3  ml-8 rounded-2xl max-w-xs mt-1 font-light',
                     `${
                       message.userId === selectedUser.id
                         ? 'bg-sorbet text-white'
@@ -83,19 +97,9 @@ export function ChatList({
                 >
                   {message.message}
                 </span>
-                {message.userId !== selectedUser.id && (
-                  <Avatar className='flex justify-center items-center'>
-                    <AvatarImage
-                      src={message.avatar}
-                      alt={message.nickname}
-                      width={6}
-                      height={6}
-                    />
-                  </Avatar>
-                )}
-              </div>
-            </motion.div>
-          ))}
+              </motion.div>
+            );
+          })}
         </AnimatePresence>
       </div>
       <div className='mt-4'>
@@ -108,3 +112,39 @@ export function ChatList({
     </div>
   );
 }
+
+/*
+  {message.userId === selectedUser.id && (
+    <Avatar className='flex justify-center items-center'>
+      <AvatarImage
+        src={message.avatar ? message.avatar : '/avatar.svg'}
+        alt={message.nickname}
+        width={6}
+        height={6}
+      />
+      <AvatarFallback>{message.nickname[0]}</AvatarFallback>
+    </Avatar>
+  )}
+  <span
+    className={cn(
+      'bg-accent p-3 mr-3 rounded-3xl max-w-xs',
+      `${
+        message.userId === selectedUser.id
+          ? 'bg-sorbet text-white'
+          : 'bg-[#D7D7D7]'
+      }`
+    )}
+  >
+    {message.message}
+  </span>
+  {message.userId !== selectedUser.id && (
+    <Avatar className='flex justify-center items-center'>
+      <AvatarImage
+        src={message.avatar}
+        alt={message.nickname}
+        width={6}
+        height={6}
+      />
+    </Avatar>
+  )}
+*/
