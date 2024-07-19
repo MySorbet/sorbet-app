c799f03813b46dcbeb6a3837a4fffa068c42a9f5
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-07-19 17:41:12 -0600

    Simplify twitter widget using composition and new design

--- src/components/profile/widgets/widget-twitter.tsx
+++ src/components/profile/widgets/widget-twitter.tsx
@@ -3,6 +3,7 @@ import {
   WidgetIcon,
   ImageOverlay,
 } from '@/components/profile/widgets/';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { TwitterWidgetContentType, WidgetSize, WidgetType } from '@/types';
@@ -22,112 +23,116 @@ export const TwitterWidget: React.FC<TwitterWidgetProps> = ({
   content,
   size,
 }) => {
-  let widgetLayout;
   switch (size) {
     case WidgetSize.A:
-      widgetLayout = (
-        <div className='h-full flex flex-col gap-2'>
+      return (
+        <WidgetLayout>
           <WidgetHeader>
-            <WidgetIcon type={WidgetType.TwitterProfile} />
-            <div className='flex-grow'>
-              <div className='text-sm font-semibold'>{content.accountName}</div>
-              <div className='text-xs text-gray-500'>
-                <TwitterProfileDescription
-                  description={content.accountDescription}
-                />
-              </div>
-            </div>
+            <WidgetIcon type={WidgetType.TwitterProfile} className='mb-0' />
             <FollowButton handle={content.accountHandle} />
           </WidgetHeader>
-          <div className='flex-grow relative rounded-xl overflow-hidden'>
-            <img
-              src={content.bannerImage}
-              alt='Twitter content'
-              className='absolute inset-0 w-full h-full object-cover'
-            />
-            <ImageOverlay />
-          </div>
-        </div>
+          <Handle handle={content.accountHandle} img={content.profileImage} />
+          {/* // TODO: does this need className='flex-grow' on root*/}
+          {/* // TODO: does this need className='absolute inset-0' on img*/}
+          <BannerImage src={content.bannerImage} />
+        </WidgetLayout>
       );
-      break;
     case WidgetSize.B:
-      widgetLayout = (
-        <div className='h-full flex flex-col gap-2'>
-          <div>
+      return (
+        <WidgetLayout>
+          <WidgetHeader>
             <WidgetIcon type={WidgetType.TwitterProfile} className='m-0' />
-          </div>
-          <div>
-            <div className='text-sm font-semibold'>{content.accountName}</div>
-            <div className='text-xs text-gray-500'>
-              <TwitterProfileDescription
-                description={content.accountDescription}
-              />
-            </div>
-          </div>
-          <div className='h-full w-full relative rounded-xl overflow-hidden'>
-            <img
-              src={content.bannerImage}
-              alt='Twitter content'
-              className='w-full h-full object-cover'
-            />
-            <ImageOverlay />
-          </div>
-        </div>
+            <FollowButton handle={content.accountHandle} />
+          </WidgetHeader>
+          <Handle handle={content.accountHandle} img={content.profileImage} />
+          <Bio bio={content.accountDescription} />
+          <BannerImage src={content.bannerImage} />
+        </WidgetLayout>
       );
-      break;
     case WidgetSize.C:
-      widgetLayout = (
-        <div className='h-full flex flex-row gap-2'>
-          <div className='flex flex-row gap-2'>
-            <div className='w-1/5'>
-              <WidgetIcon type={WidgetType.TwitterProfile} />
-            </div>
-            <div>
-              <div className='text-sm font-semibold'>{content.accountName}</div>
-              <div className='text-xs text-gray-500'>
-                {content.accountDescription}
-              </div>
-            </div>
+      return (
+        <WidgetLayout>
+          <WidgetHeader>
+            <WidgetIcon type={WidgetType.TwitterProfile} className='m-0' />
+            <FollowButton handle={content.accountHandle} />
+          </WidgetHeader>
+          <Handle handle={content.accountHandle} img={content.profileImage} />
+          <div className='flex flex-row gap-3 justify-between h-full'>
+            <Bio bio={content.accountDescription} />
+            <BannerImage src={content.bannerImage} className='w-2/3' />
           </div>
-          <div className={`relative rounded-xl overflow-hidden w-50`}>
-            <img
-              src={content.bannerImage}
-              alt='Twitter content'
-              className='w-full h-full object-cover'
-              style={{ objectFit: 'cover' }}
-            />
-            <ImageOverlay />
-          </div>
-        </div>
+        </WidgetLayout>
       );
-      break;
     case WidgetSize.D:
-      widgetLayout = (
-        <div className='h-full flex flex-col gap-2'>
-          <WidgetIcon type={WidgetType.TwitterProfile} className='m-0' />
-          <div>
-            <div className='text-sm font-semibold'>{content.accountName}</div>
-            <div className='text-xs text-gray-500'>
-              {content.accountDescription}
-            </div>
-          </div>
-          <div className={`h-full w-full relative rounded-xl overflow-hidden`}>
-            <img
-              src={content.bannerImage}
-              alt='Twitter content'
-              className='w-full h-full object-cover'
-              style={{ objectFit: 'cover' }}
-            />
-            <ImageOverlay />
-          </div>
-        </div>
+      return (
+        <WidgetLayout>
+          <WidgetHeader>
+            <WidgetIcon type={WidgetType.TwitterProfile} className='m-0' />
+            <FollowButton handle={content.accountHandle} />
+          </WidgetHeader>
+          <Handle handle={content.accountHandle} img={content.profileImage} />
+          <Bio bio={content.accountDescription} />
+          <BannerImage src={content.bannerImage} />
+        </WidgetLayout>
       );
-      break;
     default:
-      widgetLayout = <div>Unsupported widget size</div>;
+      return <div>Unsupported widget size</div>;
   }
+};
 
-  return widgetLayout;
+/**
+ * Local component to render the layout of the widget as a flex column with a gap between children.
+ */
+const WidgetLayout = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn('h-full flex flex-col gap-3', className)}
+    {...props}
+  />
+));
+
+/**
+ * Local component to render the avatar image and handle for the twitter profile.
+ */
+const Handle: React.FC<{ handle: string; img: string }> = ({ handle, img }) => {
+  return (
+    <div className='flex flex-row gap-1 items-center'>
+      <Avatar className='size-5'>
+        <AvatarImage src={img} alt={`@${handle}`} />
+        <AvatarFallback>{handle.substring(0, 1)}</AvatarFallback>
+      </Avatar>
+      <span className='text-secondary-foreground text-sm font-medium'>{`@${handle}`}</span>
+    </div>
+  );
+};
+
+/**
+ * Local component to render the banner image for the twitter profile.
+ *
+ * TODO: Use Next.js Image component for better performance.
+ */
+const BannerImage: React.FC<{ src: string; className?: string }> = ({
+  src,
+  className,
+}) => {
+  return (
+    <div
+      className={cn(
+        `h-full w-full relative rounded-2xl overflow-hidden`,
+        className
+      )}
+    >
+      <img
+        src={src}
+        alt='Banner image from twitter profile'
+        className='w-full h-full object-cover'
+      />
+      <ImageOverlay />
+    </div>
+  );
 };
 
 /**
@@ -160,15 +165,16 @@ const FollowButton: React.FC<{ handle: string }> = ({ handle }) => {
 /**
  * Local component to render the twitter profile description, truncating it if it exceeds `DESCRIPTION_CHARS_LIMIT`
  */
-const TwitterProfileDescription: React.FC<{ description: string }> = ({
-  description,
+const Bio: React.FC<{ bio: string; className?: string }> = ({
+  bio,
+  className,
 }) => {
   const DESCRIPTION_CHARS_LIMIT = 80;
   return (
-    <span>
-      {description.length > DESCRIPTION_CHARS_LIMIT
-        ? `${description.substring(0, DESCRIPTION_CHARS_LIMIT)}...`
-        : description}
+    <span className={cn('text-xs text-gray-500', className)}>
+      {bio.length > DESCRIPTION_CHARS_LIMIT
+        ? `${bio.substring(0, DESCRIPTION_CHARS_LIMIT)}...`
+        : bio}
     </span>
   );
 };
