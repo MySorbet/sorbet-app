ce95f3ee1bc202e8b3672523a59e6b13a7754939
Chris Park <cp3063@columbia.edu>
2024-07-22 00:48:58 -0700

    Feature: adds framer motion to FilePreview component

--- src/app/gigs/chat/chat-file-preview.tsx
+++ src/app/gigs/chat/chat-file-preview.tsx
@@ -1,6 +1,7 @@
+'use client';
+
+import { motion } from 'framer-motion';
 import { CircleX } from 'lucide-react';
-import Image from 'next/image';
-import { Dispatch, SetStateAction } from 'react';
 
 interface FilePreviewProps {
   file: string;
@@ -9,7 +10,20 @@ interface FilePreviewProps {
 
 const FilePreview = ({ file, removeFile }: FilePreviewProps) => {
   return (
-    <div className='relative h-12 w-16 rounded-lg overflow-hidden p-0 border border-gray-400'>
+    <motion.div
+      initial={{ scale: 0.5, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+      exit={{ scale: 0, opacity: 0 }}
+      transition={{
+        opacity: { duration: 0.02 },
+        layout: {
+          type: 'spring',
+          stiffness: 700,
+          damping: 30,
+        },
+      }}
+      className='relative h-12 w-16 rounded-lg overflow-hidden p-0 border border-gray-400'
+    >
       <button className='' onClick={() => removeFile()}>
         <CircleX className='absolute top-[1px] left-[1px] h-4 w-4 text-white z-20' />
         <div className='absolute top-[1px] left-[1px] h-4 w-4 rounded-full bg-gray-500 z-10' />
@@ -19,7 +33,7 @@ const FilePreview = ({ file, removeFile }: FilePreviewProps) => {
         alt='file'
         className='absolute top-0 w-full h-full object-cover '
       />
-    </div>
+    </motion.div>
   );
 };
 
