dd85777946ac9b064d7425a71567d217870ce503
Chris Park <cp3063@columbia.edu>
2024-07-22 23:13:48 -0700

    Feature: WARNING TLDR:  fixes download for files that are sent by the user. Sendbird file url is not accessible in the 'onMessagesAdded' callback though it is present in the console.log. What I did was basically was for user inputted files, while the current state of the chat-layout-minimal is the same, I had to create a blob froom the file. When the chat-layout-minimal component re-renders, we render based on the state that is set from fetching messages from sendbird. Can definitely explain better if needed

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -1,14 +1,16 @@
 import axios from 'axios';
 
 export async function fetchFile(sendbirdUrl: string, type: string) {
-  if (sendbirdUrl.includes('blob:')) return sendbirdUrl.slice(5);
+  if (sendbirdUrl.includes('blob:')) return sendbirdUrl;
 
   const headers = {
     'Api-Token': process.env.NEXT_PUBLIC_SEND_BIRD_TOKEN,
   };
 
   try {
-    const response = await fetch(sendbirdUrl, { headers });
+    const response = await fetch(sendbirdUrl, {
+      headers,
+    });
     console.log('response ', response);
     const bufferArray = await response.arrayBuffer();
     const blob = new Blob([bufferArray], { type: type });
