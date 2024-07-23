8dc28188747c7ae7aa4294e106e5719a4a120259
Chris Park <cp3063@columbia.edu>
2024-07-23 14:21:35 -0700

    Refactor: implements fetching sendbird file messages in the backend and then receiving the binary array buffer on the front end. Did this so the Sendbird secret api key is not exposed on the client

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -1,20 +1,24 @@
-'use server';
+// 'use server';
+import { API_URL, validateToken } from '@/utils';
+import axios from 'axios';
+import * as blobUtil from 'blob-util';
 
 export async function fetchFile(sendbirdUrl: string, type: string) {
   if (sendbirdUrl.includes('blob:')) {
     return sendbirdUrl;
   }
-
-  const headers = {
-    'Api-Token': process.env.SEND_BIRD_TOKEN || '',
-  };
+  const reqBody = { url: sendbirdUrl };
+  const reqHeaders = validateToken({}, true);
 
   try {
-    const response = await fetch(sendbirdUrl, {
-      headers,
-    });
-    const bufferArray = await response.arrayBuffer();
-    const blob = new Blob([bufferArray], { type: type });
+    const response = await axios.post(
+      `${API_URL}/images/sendbird`,
+      reqBody,
+      reqHeaders
+    );
+    const binary = response.data.binString;
+    const buffer = blobUtil.binaryStringToArrayBuffer(binary);
+    const blob = new Blob([buffer], { type: type });
     const blobUrl = URL.createObjectURL(blob);
     return blobUrl;
   } catch (error: any) {
