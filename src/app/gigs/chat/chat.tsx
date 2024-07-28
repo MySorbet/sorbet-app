c917ababa5878de0e7bd58b50c4f9c9b1beff7e2
Christopher Park <113001736+ChristopherPark3@users.noreply.github.com>
2024-07-27 21:45:24 -0700

    u/cp/sendbird knock (#37)
    # Description & Technical Solution
    ** STILL NEEDS TO BE MERGED WITH THE `u/cp/claim-sorbet` repo. Waiting
    for that to be reviewed.
    ** COMMITS START JULY 25TH
    
    - Implements users receiving message notifications when they are away
    from chat.
    - Note, double messages will appear in dev because of React strict mode.
    I tested in prod and only one notification appeared.
    - The cause is because I am running an effect to check whenever the
    `Dialog` component opens and closes. When it closes, I then disconnect
    from the Sendbird server.
    - Also implements ui changes from Chris Astles in regards to the display
    of file messages.
    
    
    Describe problems, if any, clearly and concisely.
    - The only problem that I see is the Knock messages not appearing how I
    expect. I think it has something to do with the Knock workflow and
    editing the message in the Knock dashboard should fix it.
    
    Summarize the impact to the system.
    - Allows users to receive message notifications when they are away from
    their keyboard
    
    Please also include relevant motivation and context.
    - Incorporate messaging into Knock so that users can always know when
    others respond or reach out.
    
    Please include a summary of the technical solution and how it solves the
    problem.
    - Everytime a user closes out of a chat, we are considering them
    `offline` and do so by disconnecting from the Sendbird servers. We do
    this so that the 'connectionStatus' for each Sendbird User object is
    properly updated.
    - Then, whenever a new message is added, we run a check to see if the
    other user is `offline` and if so, we make a call to our backend to
    trigger the Knock workflow to send a notification.
    
    # Checklist
    
    - [x] I have commented my code, particularly in hard-to-understand
    areas.
    - [x] Already rebased against main branch.
    
    # Screenshots
    
    Provide screenshots or videos of the changes made if any.
    
    UI changes for file messages:
    <img width="839" alt="Screenshot 2024-07-25 at 7 47 29 PM"
    src="https://github.com/user-attachments/assets/198779c6-6d14-4bcb-ad12-b15eb066a1c7">
    
    
    What the notification text looks like in app:
    <img width="393" alt="Screenshot 2024-07-25 at 7 48 05 PM"
    src="https://github.com/user-attachments/assets/e14a5a9c-4d2f-47c5-befb-d03ac8f6bd3b">
    
    
    What it looks like in the Knock dashboard: 
    <img width="423" alt="Screenshot 2024-07-25 at 7 48 59 PM"
    src="https://github.com/user-attachments/assets/f73ea5a3-4232-44c1-92b1-b74904474a66">

--- src/app/gigs/chat/chat.tsx
+++ src/app/gigs/chat/chat.tsx
@@ -1,11 +1,24 @@
 import ChatBottombar from './chat-bottombar';
 import { ChatList } from './chat-list';
 import ChatTopbar from './chat-topbar';
-import { User } from '@/types';
-import { SBMessage, SendMessageParams } from '@/types/sendbird';
+import { ContractStatus, User } from '@/types';
+import {
+  SBMessage,
+  SendMessageParams,
+  SupportedFileIcons,
+} from '@/types/sendbird';
 import { GroupChannel, Member } from '@sendbird/chat/groupChannel';
 import { motion } from 'framer-motion';
+import { File, ImageIcon } from 'lucide-react';
 import React, { Dispatch, SetStateAction } from 'react';
+
+const icons = {
+  pdf: <File className='w-5 h-5 text-white' />,
+  jpeg: <ImageIcon className='w-5 h-5 text-white' />,
+  png: <ImageIcon className='w-5 h-5 text-white' />,
+};
+
+type SupportedFileIcon = keyof typeof icons;
 
 interface ChatProps {
   messages: SBMessage[];
@@ -14,6 +27,7 @@ interface ChatProps {
   showTopbar?: boolean;
   channel: GroupChannel | undefined | null;
   typingMembers: Member[];
+  contractStatus: string;
 }
 
 export function Chat({
@@ -23,6 +37,7 @@ export function Chat({
   showTopbar = true,
   channel,
   typingMembers,
+  contractStatus,
 }: ChatProps) {
   const sendMessage = (newMessage: SendMessageParams) => {
     if (!channel) return;
@@ -61,12 +76,16 @@ export function Chat({
         messages={messages}
         selectedUser={selectedUser}
         typingMembers={typingMembers as Member[]}
+        contractStatus={contractStatus}
+        supportedIcons={icons as SupportedFileIcons}
       />
       <div className='mt-4'>
         <ChatBottombar
           sendMessage={sendMessage}
           isMobile={isMobile}
           channel={channel}
+          contractStatus={contractStatus}
+          supportedIcons={icons as SupportedFileIcons}
         />
       </div>
     </div>
