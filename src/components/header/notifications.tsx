a72fe483c3602df1ac5f637fa806634e08d8ca0d
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-08-01 15:14:30 -0600

    Fix build using "git config core.ignorecase false"

---
+++ src/components/header/notifications.tsx
@@ -0,0 +1,34 @@
+import './notifications.css';
+import { NotificationToasts } from '@/components/notifications';
+import {
+  KnockFeedProvider,
+  NotificationFeedPopover,
+  NotificationIconButton,
+} from '@knocklabs/react';
+import React, { useRef, useState } from 'react';
+
+export const Notifications = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const notifButtonRef = useRef(null);
+
+  return (
+    <div className='cursor-pointer'>
+      <KnockFeedProvider
+        feedId={process.env.NEXT_PUBLIC_KNOCK_FEED_CHANNEL_ID || ''}
+      >
+        <>
+          <NotificationIconButton
+            ref={notifButtonRef}
+            onClick={(e) => setIsVisible(!isVisible)}
+          />
+          <NotificationFeedPopover
+            buttonRef={notifButtonRef}
+            isVisible={isVisible}
+            onClose={() => setIsVisible(false)}
+          />
+        </>
+        <NotificationToasts />
+      </KnockFeedProvider>
+    </div>
+  );
+};
