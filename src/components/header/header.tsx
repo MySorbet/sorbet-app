81e31bce6196f5d54adb27d01f539e5d099a3cff
Chris Park <cp3063@columbia.edu>
2024-08-05 12:47:28 -0700

    Minor: runs this command 'git config core.ignorecase false' to ensure github doesn't ignore casing. ('header' vs 'Header')

---
+++ src/components/header/header.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+/* eslint-disable @next/next/no-img-element */
+import { Notifications } from '@/components/header/notifications';
+import { Button } from '@/components/ui/button';
+import { useAuth } from '@/hooks';
+import { useAppDispatch } from '@/redux/hook';
+import { setOpenSidebar } from '@/redux/userSlice';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+interface HeaderProps {
+  isPublic?: boolean;
+}
+
+export const Header = ({ isPublic = false }: HeaderProps) => {
+  const { user } = useAuth();
+
+  const dispatch = useAppDispatch();
+
+  const router = useRouter();
+
+  return (
+    <div className='bg-[#F2F3F7]'>
+      <div className='flex w-full justify-between container mx-auto py-4'>
+        <div className='flex gap-6'>
+          <Link href='/'>
+            <img
+              src='https://i.imgur.com/CbbXxed.png'
+              alt='logo'
+              width={44}
+              height={44}
+            />
+          </Link>
+        </div>
+        {!isPublic && (
+          <div className='flex items-center justify-end gap-4'>
+            <div className='flex flex-row align-center gap-2 items-center'>
+              <Notifications />
+              <div>
+                <img
+                  src='/images/menu.svg'
+                  alt='menu'
+                  className='cursor-pointer p-[10px]'
+                  onClick={() => dispatch(setOpenSidebar(true))}
+                />
+              </div>
+            </div>
+          </div>
+        )}
+        {isPublic && !user && (
+          <div className='flex items-center justify-end gap-4'>
+            <Button
+              onClick={() => router.push('/signup')}
+              className='font-semibold text-sm text-[#344054] leading-5 py-[10px] px-[14px] bg-white border border-[#D0D5DD] hover:bg-gray-100'
+            >
+              Claim my Sorbet
+            </Button>
+            <Button
+              onClick={() => router.push('/signin')}
+              className='font-semibold text-sm text-white leading-5 py-[10px] px-[14px] bg-sorbet border border-[#7F56D9] hover:bg-[#523BDF]'
+            >
+              Login
+            </Button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
