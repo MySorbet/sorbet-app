f4b10ab1535bcd38890b656cb4357f280ace5b2b
Chris Park <cp3063@columbia.edu>
2024-08-13 13:55:07 -0700

    Refactor: adds proper argument to clipboard

--- src/components/profile/profile-header.tsx
+++ src/components/profile/profile-header.tsx
@@ -1,6 +1,10 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Button } from '@/components/ui/button';
-import { Popover, PopoverTrigger, PopoverContent } from '@/components/ui/popover';
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@/components/ui/popover';
 import { User } from '@/types';
 import { SquareArrowOutUpRight } from 'lucide-react';
 import React from 'react';
@@ -23,13 +27,13 @@ export const ProfileHeader: React.FC<ProfileHeaderProps> = ({
   const handleUrlToClipboard = () => () => {
     const username = user.accountId.split('.')[0];
     const url = `${window.location.origin}/${username}`;
-    navigator.clipboard.writeText(window.location.href);
+    navigator.clipboard.writeText(url);
   };
 
   return (
     <>
       <div className='flex justify-center'>
-        <Avatar className={`w-20 h-20`}>
+        <Avatar className={`h-20 w-20`}>
           <AvatarImage
             src={
               !user.profileImage || user.profileImage === ''
@@ -48,14 +52,14 @@ export const ProfileHeader: React.FC<ProfileHeaderProps> = ({
       </div>
       <div className='flex justify-center'>
         <div className='lg:w-5/12'>
-          <h1 className='text-3xl font-semibold text-center'>{user.bio}</h1>
+          <h1 className='text-center text-3xl font-semibold'>{user.bio}</h1>
         </div>
       </div>
-      <div className='md:flex md:flex-row lg:flex lg:flex-row gap-2 justify-center grid grid-cols-2 lg:grid-cols-none'>
+      <div className='grid grid-cols-2 justify-center gap-2 md:flex md:flex-row lg:flex lg:grid-cols-none lg:flex-row'>
         {user.tags &&
           user.tags.map((tag: string) => (
             <span
-              className='border border-1 border-gray-400 py-1 px-2 rounded-full text-xs text-center'
+              className='border-1 rounded-full border border-gray-400 px-2 py-1 text-center text-xs'
               key={tag}
             >
               {tag}
@@ -64,8 +68,8 @@ export const ProfileHeader: React.FC<ProfileHeaderProps> = ({
       </div>
       <div>
         {(!user.firstName || !user.lastName || !user.bio) && canEdit && (
-          <div className='w-full flex justify-center'>
-            <div className='text-sorbet border border-1 border-dashed border-sorbet rounded-lg p-4'>
+          <div className='flex w-full justify-center'>
+            <div className='text-sorbet border-1 border-sorbet rounded-lg border border-dashed p-4'>
               <i>
                 You can start adding your profile info using the Edit Profile
                 button below
@@ -75,7 +79,7 @@ export const ProfileHeader: React.FC<ProfileHeaderProps> = ({
         )}
       </div>
       {user && (
-        <div className='flex flex-row gap-6 justify-center mt-4 items-center'>
+        <div className='mt-4 flex flex-row items-center justify-center gap-6'>
           {canEdit ? (
             <Button
               variant='outline'
@@ -96,7 +100,7 @@ export const ProfileHeader: React.FC<ProfileHeaderProps> = ({
           <Popover>
             <PopoverTrigger>
               <Button
-                className='text-[#573DF5] flex flex-row align-center gap-1 items-center bg-inherit hover:bg-inherit'
+                className='align-center flex flex-row items-center gap-1 bg-inherit text-[#573DF5] hover:bg-inherit'
                 onClick={handleUrlToClipboard()}
               >
                 <SquareArrowOutUpRight size={16} />
