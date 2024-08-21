87e4aad8bb6a5b7fb4524dbba263c2d627d6310e
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-08-21 17:48:49 -0600

    update imports and remove comments

--- src/app/container.tsx
+++ src/app/container.tsx
@@ -1,16 +1,13 @@
 'use client';
 
+import { usePrivy } from '@privy-io/react-auth';
 import { useRouter } from 'next/navigation';
 import { type FC, PropsWithChildren, useEffect } from 'react';
-
-import { Loading } from '@/components/common';
-import { useAuth } from '@/hooks/useAuth';
-import { getAccessToken, usePrivy } from '@privy-io/react-auth';
 
 const Container: FC<PropsWithChildren> = ({ children }) => {
   // const { user, accessToken, checkAuth, appLoading, logout } = useAuth();
 
-  const { ready, authenticated, user, logout } = usePrivy();
+  const { ready, authenticated } = usePrivy();
 
   const router = useRouter();
 
@@ -19,20 +16,6 @@ const Container: FC<PropsWithChildren> = ({ children }) => {
       router.push('/');
     }
   }, [ready, authenticated, router]);
-
-  // useEffect(() => {
-  //   (async () => {
-  //     if (accessToken) {
-  //       const user = await checkAuth();
-  //       if (!user) {
-  //         logout();
-  //         router.push('/signin');
-  //       }
-  //     } else {
-  //       router.push('/signin');
-  //     }
-  //   })();
-  // }, [router, accessToken]);
 
   return <>{children}</>;
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
 'use client';
 
-import Head from './head';
-import { Toaster } from '@/components/ui/toaster';
-import { AuthProvider } from '@/hooks/useAuth';
-import { store } from '@/redux/store';
 import '@/styles/colors.css';
 import '@/styles/globals.css';
+
 import { PrivyProvider } from '@privy-io/react-auth';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
+
+import { Toaster } from '@/components/ui/toaster';
+import { AuthProvider } from '@/hooks/useAuth';
+import { store } from '@/redux/store';
+
+import Head from './head';
 
 const queryClient = new QueryClient();
 
