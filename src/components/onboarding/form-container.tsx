2bc4ec3b9a9193011a88cb77b92e54c3609c241a
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-08-23 12:57:21 -0600

    Animate form container

--- src/components/onboarding/form-container.tsx
+++ src/components/onboarding/form-container.tsx
@@ -4,13 +4,10 @@ import { FC, PropsWithChildren } from 'react';
 
 const FormContainer: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div
-      style={{
-        boxShadow: '0px 20px 60px 0px #20202026',
-      }}
-      className='flex w-[400px] flex-col gap-6 rounded-3xl bg-[#F9F7FF] p-6'
-    >
-      {children}
+    <div className='flex w-[400px] flex-col gap-6 rounded-3xl bg-[#F9F7FF] p-6 shadow-lg'>
+      <div className='animate-in slide-in-from-right-3 fade-in-50 size-full'>
+        {children}
+      </div>
     </div>
   );
 };
