01fd35a328f864edc0cfec1fe8c09ff97f70ed77
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-09-03 14:56:46 -0600

    Rename splash logo and comment

--- src/app/splash.tsx
+++ src/app/splash.tsx
@@ -2,12 +2,23 @@
 
 import { usePrivy } from '@privy-io/react-auth';
 import { useRouter } from 'next/navigation';
-import { FC, PropsWithChildren, useEffect } from 'react';
+import { FC, useEffect } from 'react';
 
 import { useAuth } from '@/hooks';
-import MutedLogo from '~/muted-logo.svg';
+import MutedSorbetLogo from '~/muted-sorbet-logo.svg';
 
-const Splash: FC<PropsWithChildren> = (props) => {
+/**
+ * Renders a full page splash screen displaying a muted Sorbet logo that fades in and up - gmail style.
+ *
+ * Should be used to display a splash screen while the user is redirected to the appropriate page
+ * based on their authentication status.
+ *
+ * 1. Wait until privy is ready
+ * 2. If authenticated and there is a user, redirect to the user's handle
+ * 3. If authenticated and there is no user, do nothing and console error
+ * 4. If not authenticated, redirect to signin
+ */
+const Splash: FC = () => {
   const { ready, authenticated } = usePrivy();
   const router = useRouter();
   const { user } = useAuth();
@@ -29,7 +40,7 @@ const Splash: FC<PropsWithChildren> = (props) => {
 
   return (
     <div className='flex h-full w-full items-center justify-center'>
-      <MutedLogo className='animate-in slide-in-from-bottom-10 fade-in-10 duration-5000 size-44' />
+      <MutedSorbetLogo className='animate-in slide-in-from-bottom-10 fade-in-10 duration-5000 size-44' />
     </div>
   );
 };
