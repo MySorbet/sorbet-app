5a4a7b8d381be1ccc97357c060fe542310b5b4d0
Wonder Boy <wonder725boy@gmail.com>
2024-09-06 05:48:51 +0800

    update fetch balance of wallet

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 export * from './gigs';
 export * from './profile';
 export { useAuth } from './useAuth';
+export { useWalletBalances } from './useWeb3React';
 export { useLocalStorage } from './useLocalStorage';
 export * from './widgets';
