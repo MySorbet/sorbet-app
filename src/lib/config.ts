a492555f8cef2aa0ea52fc79ccb5ee4f4bb927f9
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-09-06 11:42:07 -0600

    Add t3-oss/env and remove unused logger

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,10 +1,10 @@
+import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
 // Zod schema for the app configuration
 const appConfigSchema = z.object({
   nodeEnv: z.enum(['development', 'production', 'test']).default('development'),
   sorbetApiUrl: z.string().url().optional().default('http://localhost:6200'),
-  showLogger: z.preprocess((val) => val === 'true', z.boolean()).default(false),
   googleMapKey: z.string(),
   sendbirdAppId: z.string(),
   privyAppId: z.string(),
@@ -18,10 +18,31 @@ type AppConfig = z.infer<typeof appConfigSchema>;
 export const config: AppConfig = appConfigSchema.parse({
   nodeEnv: process.env.NEXT_PUBLIC_NODE_ENV,
   sorbetApiUrl: process.env.NEXT_PUBLIC_SORBET_API_URL,
-  showLogger: process.env.NEXT_PUBLIC_SHOW_LOGGER === 'true',
   googleMapKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY,
   sendbirdAppId: process.env.NEXT_PUBLIC_SEND_BIRD_APP_ID,
   privyAppId: process.env.NEXT_PUBLIC_PRIVY_APP_ID,
   contractAddress: process.env.NEXT_PUBLIC_BASE_CONTRACT_ADDRESS,
   usdcAddress: process.env.NEXT_PUBLIC_BASE_USDC_ADDRESS,
+});
+
+export const env = createEnv({
+  client: {
+    NEXT_PUBLIC_NODE_ENV: z.enum(['development', 'production', 'test']),
+    NEXT_PUBLIC_SORBET_API_URL: z.string().url(),
+    NEXT_PUBLIC_GOOGLE_MAP_KEY: z.string(),
+    NEXT_PUBLIC_SEND_BIRD_APP_ID: z.string(),
+    NEXT_PUBLIC_PRIVY_APP_ID: z.string(),
+    NEXT_PUBLIC_BASE_CONTRACT_ADDRESS: z.string(),
+    NEXT_PUBLIC_BASE_USDC_ADDRESS: z.string(),
+  },
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
+    NEXT_PUBLIC_SORBET_API_URL: process.env.NEXT_PUBLIC_SORBET_API_URL,
+    NEXT_PUBLIC_GOOGLE_MAP_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY,
+    NEXT_PUBLIC_SEND_BIRD_APP_ID: process.env.NEXT_PUBLIC_SEND_BIRD_APP_ID,
+    NEXT_PUBLIC_PRIVY_APP_ID: process.env.NEXT_PUBLIC_PRIVY_APP_ID,
+    NEXT_PUBLIC_BASE_CONTRACT_ADDRESS:
+      process.env.NEXT_PUBLIC_BASE_CONTRACT_ADDRESS,
+    NEXT_PUBLIC_BASE_USDC_ADDRESS: process.env.NEXT_PUBLIC_BASE_USDC_ADDRESS,
+  },
 });
