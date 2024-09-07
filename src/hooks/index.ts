e4e9a35178f3a7240dbe63bd5df7186321ddaf74
Wonder Boy <152345719+wonderboy25@users.noreply.github.com>
2024-09-07 09:52:14 +0800

    Wallet Page Base Migration (#67)
    # Wallet Page Base Migration
    - Show wallet balance on wallet page
    - stop backend call for transaction
    - add NEXT_PUBLIC_BASE_RPC_URL on env

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 export * from './gigs';
 export * from './profile';
 export { useAuth } from './useAuth';
+export { useWalletBalances } from './useWalletBalances';
 export { useLocalStorage } from './useLocalStorage';
 export * from './widgets';
