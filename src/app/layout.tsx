3620d94d35027bf4eb004da274e98bb3469a62a1
Christopher Park <christopherjcpark00@gmail.com>
2024-09-17 15:53:18 -0700

    Feature: updates metadata object to include keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import '@/styles/colors.css';
 import '@/styles/globals.css';
 
+import { Metadata } from 'next';
+
 import { ClientProviders } from '@/app/client-providers';
 
 import Head from './head';
 
 /** Docs on metadata object options: https://nextjs.org/docs/app/api-reference/functions/generate-metadata#metadata-fields */
-export const metadata = {
+export const metadata: Metadata = {
   metadataBase: new URL('https://www.mysorbet.io'),
   title: {
     default: 'Sorbet | The All-in One Payment Experience for Freelancers',
@@ -14,6 +16,13 @@ export const metadata = {
   },
   description:
     'Unlock your global creative potential with Sorbet. A secure and trustworthy tool here to support you throughout your freelancing journey.',
+  keywords: [
+    'social network',
+    'BASE blockchain',
+    'collaboration',
+    'creators',
+    'ai',
+  ],
 };
 
 export default function RootLayout({
