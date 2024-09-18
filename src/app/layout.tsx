9081a10cc163c53c7da921dea8f94f4d66951ff4
Christopher Park <113001736+ChristopherPark3@users.noreply.github.com>
2024-09-18 14:54:24 -0700

    Adds back metadata that was lost in merge conflict (#84)
    # Description & Technical Solution
    
    # Screenshots
    
    Provide screenshots or videos of the changes made if any (optional)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,31 @@
 import '@/styles/colors.css';
 import '@/styles/globals.css';
 
+import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import { ClientProviders } from '@/app/client-providers';
 import { cn } from '@/lib/utils';
 
 import Head from './head';
+
+/** Docs on metadata object options: https://nextjs.org/docs/app/api-reference/functions/generate-metadata#metadata-fields */
+export const metadata: Metadata = {
+  metadataBase: new URL('https://www.mysorbet.io'),
+  title: {
+    default: 'Sorbet | The All-in One Payment Experience for Freelancers',
+    template: 'Sorbet | %s',
+  },
+  description:
+    'Unlock your global creative potential with Sorbet. A secure and trustworthy tool here to support you throughout your freelancing journey.',
+  keywords: [
+    'social network',
+    'BASE blockchain',
+    'collaboration',
+    'creators',
+    'ai',
+  ],
+};
 
 const inter = Inter({
   subsets: ['latin'],
