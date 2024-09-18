0ffbd0308db828772741d74f7a8afa392c392c3f
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-09-18 10:24:41 -0600

    Use CopyButton in share-link, size icon correctly, and truncate text correctly

--- src/components/profile/share/components/share-link.tsx
+++ src/components/profile/share/components/share-link.tsx
@@ -1,11 +1,9 @@
 'use client';
 
-import { CheckCircle, Copy05 } from '@untitled-ui/icons-react';
 import Image from 'next/image';
-import { useState } from 'react';
 
 import Logo from '@/../public/images/logo.png';
-import { Button } from '@/components/ui/button';
+import { CopyButton } from '@/components/common';
 
 export const ShareLink = ({
   username,
@@ -14,36 +12,27 @@ export const ShareLink = ({
   username: string;
   handleUrlToClipboard: () => void;
 }) => {
-  const [isCopied, setIsCopied] = useState(false);
-
-  const hostname = window.location.hostname
-  
+  const hostname = window.location.hostname;
 
   return (
     <div className='flex h-[68px] w-full items-center justify-between rounded-md border-2 border-[#D0D5DD] bg-[#FFFFFF] px-5'>
       <div className='flex items-center gap-3'>
-        <Image src={Logo} height={32} width={32} alt='logo' />
-        <p className='text-base font-medium text-[#101828]'>
-          {username.length > 10
-            ? `${hostname}/${username.slice(0, 10)}...`
-            : `${hostname}/${username}`}
+        <Image
+          src={Logo}
+          height={32}
+          width={32}
+          className='size-8'
+          alt='Sorbet logo'
+        />
+        <p className='truncate text-base font-medium text-[#101828]'>
+          {`${hostname}/${username}`}
         </p>
       </div>
-
-      <Button
-        className='group m-0 border-none bg-transparent p-0 hover:bg-transparent'
-        onClick={() => {
-          setIsCopied(true);
-          handleUrlToClipboard();
-        }}
-        disabled={isCopied}
-      >
-        {isCopied ? (
-          <CheckCircle className='h-6 w-6 text-green-500' />
-        ) : (
-          <Copy05 className='h-6 w-6 text-[#101828] ease-out group-hover:scale-105 ' />
-        )}
-      </Button>
+      <CopyButton
+        onCopy={handleUrlToClipboard}
+        copyIconClassName='size-7 text-[#101828]'
+        checkIconClassName='size-7'
+      />
     </div>
   );
 };
