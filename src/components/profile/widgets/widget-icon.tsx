213a8cc033dd0ab583d868f5b038598466681753
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-09-19 17:02:05 -0600

    Replace social icons with SVG Add WidgetTypeWithIcon type to restrict which WidgetTypes have icons Update icons in share dialog feature

--- src/components/profile/widgets/widget-icon.tsx
+++ src/components/profile/widgets/widget-icon.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 import { cn } from '@/lib/utils';
-import { WidgetType } from '@/types';
+import { WidgetType, WidgetTypes } from '@/types';
 
 /**
  * This type allows you to passthrough any prop that a Next `<Image/>` takes save for `src` which is handled by the `type` prop.
@@ -9,7 +9,7 @@ import { WidgetType } from '@/types';
 interface WidgetIconProps
   extends Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'> {
   /** The type of widget icon to render. */
-  type: WidgetType;
+  type: WidgetTypeWithIcon;
   /** Optional alt for the image. If undefined, a generic default will be used */
   alt?: string;
 }
@@ -36,30 +36,40 @@ export const WidgetIcon: React.FC<WidgetIconProps> = ({
   );
 };
 
+/** This is the subset of `WidgetType`'s that have an associated social icon. */
+export type WidgetTypeWithIcon = Exclude<WidgetType, 'Photo' | 'Link'>;
+
+/** This is the subset of `WidgetType`'s that have an associated social icon (as an array) */
+export const WidgetTypesWithIcons: WidgetTypeWithIcon[] = WidgetTypes.filter(
+  (type) => {
+    return type !== 'Photo' && type !== 'Link';
+  }
+);
+
 /**
  * Maps widget type to social icon.
  *
  * @param widgetType - The type of the widget.
  * @returns The path to the social icon image to be used as `src`
  */
-export const getSocialIconForWidget = (widgetType: WidgetType): string => {
-  const iconMap: Record<WidgetType, string> = {
-    Photo: 'default.png',
-    Substack: 'substack.png',
-    SpotifySong: 'spotify.png',
-    SpotifyAlbum: 'spotify.png',
-    SoundcloudSong: 'soundCloud.png',
-    InstagramPost: 'instagram.png',
-    InstagramProfile: 'instagram.png',
-    TwitterProfile: 'twitter.png',
-    LinkedInProfile: 'linkedIn.png',
-    Youtube: 'youtube.png',
-    Github: 'github.png',
-    Dribbble: 'dribbble.png',
-    Behance: 'behance.png',
-    Medium: 'medium.png',
-    Figma: 'figma.png',
-    Link: 'default.png',
+export const getSocialIconForWidget = (
+  widgetType: WidgetTypeWithIcon
+): string => {
+  const iconMap: Record<WidgetTypeWithIcon, string> = {
+    Substack: 'substack.svg',
+    SpotifySong: 'spotify.svg',
+    SpotifyAlbum: 'spotify.svg',
+    SoundcloudSong: 'soundCloud.svg',
+    InstagramPost: 'instagram.svg',
+    InstagramProfile: 'instagram.svg',
+    TwitterProfile: 'twitter.svg',
+    LinkedInProfile: 'linkedin.svg',
+    Youtube: 'youtube.svg',
+    Github: 'github.svg',
+    Dribbble: 'dribbble.svg',
+    Behance: 'behance.svg',
+    Medium: 'medium.svg',
+    Figma: 'figma.svg',
   };
 
   return `/images/social/${iconMap[widgetType]}`;
