f48cce82aaa8f78179f549ad7bf3736c3a1a6bc2
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-09-19 11:16:37 -0600

    Pair down sb setup for now

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,15 +1,11 @@
 import type { StorybookConfig } from '@storybook/nextjs';
-import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y'
-
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/nextjs',
