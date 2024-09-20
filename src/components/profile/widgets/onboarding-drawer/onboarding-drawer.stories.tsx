b6fb66d02b356bc103ba6efc7581866123663840
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-09-20 17:26:56 -0600

    Use @storybook/preview-api instead

--- src/components/profile/widgets/onboarding-drawer/onboarding-drawer.stories.tsx
+++ src/components/profile/widgets/onboarding-drawer/onboarding-drawer.stories.tsx
@@ -1,4 +1,4 @@
-import { useArgs } from '@storybook/client-api';
+import { useArgs } from '@storybook/preview-api';
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
