43b905c2f2dcb3a6144d0c8e42a0666a1e7d8e5e
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-09-20 13:38:40 -0600

    [SRBT-110] Update Widget Icons (#90)
    # [SRBT-110] Update Widget Icons
    
    - Remove unused icons and images
    - Move `getSocialIconForWidget` to `widget-icon` (since it is the only
    component that uses it)
    - Replace social icons with latest SVG version (exported from figma)
    - Prevent `WidgetIcon` from accepting "Link" or "Photo" types since
    these do not need an icon.
    - Update icons in share dialog feature
    - Add ShareProfileDialog Story (to verify icons looks right)
    
    # Screenshots
    
    <img width="853" alt="image"
    src="https://github.com/user-attachments/assets/20a7ced6-6ba4-4e91-b6d4-c8db185933ea">
    <img width="853" alt="image"
    src="https://github.com/user-attachments/assets/24ffed59-3ea6-4d6f-aeaa-d089aef08a5e">

--- src/components/profile/share/components/twitter-icon.tsx
+++ src/components/profile/share/components/twitter-icon.tsx
@@ -1,14 +1,7 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-import Twitter from '@/../public/images/social/twitter.png';
+import Twitter from '~/images/social/twitter.svg';
 
 export const TwitterIcon = () => {
-  return (
-    <Image
-    src={Twitter}
-    height={52}
-    width={52}
-    alt='Twiiter logo'
-  />
-  )
-}
+  return <Image src={Twitter} height={52} width={52} alt='Twitter logo' />;
+};
--- src/components/profile/widgets/widget-icon.tsx
+++ src/components/profile/widgets/widget-icon.tsx
@@ -1,9 +1,7 @@
 import Image from 'next/image';
 
 import { cn } from '@/lib/utils';
-import { WidgetType } from '@/types';
-
-import { getSocialIconForWidget } from './util';
+import { WidgetType, WidgetTypes } from '@/types';
 
 /**
  * This type allows you to passthrough any prop that a Next `<Image/>` takes save for `src` which is handled by the `type` prop.
@@ -11,7 +9,7 @@ import { getSocialIconForWidget } from './util';
 interface WidgetIconProps
   extends Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'> {
   /** The type of widget icon to render. */
-  type: WidgetType;
+  type: WidgetTypeWithIcon;
   /** Optional alt for the image. If undefined, a generic default will be used */
   alt?: string;
 }
@@ -36,4 +34,43 @@ export const WidgetIcon: React.FC<WidgetIconProps> = ({
       {...rest}
     />
   );
+};
+
+/** This is the subset of `WidgetType`'s that have an associated social icon. */
+export type WidgetTypeWithIcon = Exclude<WidgetType, 'Photo' | 'Link'>;
+
+/** This is the subset of `WidgetType`'s that have an associated social icon (as an array) */
+export const WidgetTypesWithIcons: WidgetTypeWithIcon[] = WidgetTypes.filter(
+  (type) => {
+    return type !== 'Photo' && type !== 'Link';
+  }
+);
+
+/**
+ * Maps widget type to social icon.
+ *
+ * @param widgetType - The type of the widget.
+ * @returns The path to the social icon image to be used as `src`
+ */
+export const getSocialIconForWidget = (
+  widgetType: WidgetTypeWithIcon
+): string => {
+  const iconMap: Record<WidgetTypeWithIcon, string> = {
+    Substack: 'substack.svg',
+    SpotifySong: 'spotify.svg',
+    SpotifyAlbum: 'spotify.svg',
+    SoundcloudSong: 'soundcloud.svg',
+    InstagramPost: 'instagram.svg',
+    InstagramProfile: 'instagram.svg',
+    TwitterProfile: 'twitter.svg',
+    LinkedInProfile: 'linkedin.svg',
+    Youtube: 'youtube.svg',
+    Github: 'github.svg',
+    Dribbble: 'dribbble.svg',
+    Behance: 'behance.svg',
+    Medium: 'medium.svg',
+    Figma: 'figma.svg',
+  };
+
+  return `/images/social/${iconMap[widgetType]}`;
 };
