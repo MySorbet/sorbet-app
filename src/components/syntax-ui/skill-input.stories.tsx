b7d7561a2c8c6243cece1732aa40acac19454d9a
Christopher Park <113001736+ChristopherPark3@users.noreply.github.com>
2024-09-23 13:36:19 -0700

    SRBT-188 SRBT-213 Character limits (#89)
    # Description & Technical Solution
    - Implements character limits for `bio` in signup and skill inputs
    - Creates `stories` for `step2.tsx` and `skill-input.tsx`
    - Changes animation trigger in `skill-input` from every time a user
    types to every time a user tries to submit
    - Creates constants file located in `/src/constant` folder
    - Installs `@storybook/jest` and `@storybook/testing-library`
    
    
    # New animation triggers
    
    <strong>Max num of skills</strong>
    
    
    https://github.com/user-attachments/assets/b907b5ec-946e-4db5-b8f9-4cdea1c566ac
    
    <strong>Max num of chars</strong>
    
    
    https://github.com/user-attachments/assets/432e5b59-c322-462d-abc2-f667181c8ec7

---
+++ src/components/syntax-ui/skill-input.stories.tsx
@@ -0,0 +1,42 @@
+import { Meta } from '@storybook/react';
+import { userEvent, within } from '@storybook/test';
+
+import SkillInput from '@/components/syntax-ui/skill-input';
+
+const meta = {
+  component: SkillInput,
+  parameters: { layout: 'centered' },
+  args: {
+    initialSkills: [],
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <div className='flex w-[400px] flex-col gap-6 rounded-3xl bg-[#F9F7FF] p-6 shadow-lg'>
+          <Story />
+        </div>
+      );
+    },
+  ],
+} satisfies Meta<typeof SkillInput>;
+
+export default meta;
+
+export const Default = {};
+
+export const MaxSkills = {
+  args: {
+    initialSkills: ['Skill 1', 'Skill 2', 'Skill 3', 'Skill 4', 'Skill 5'],
+    unique: true,
+  },
+};
+
+export const MaxCharacters = {
+  play: async ({ canvasElement }: { canvasElement: HTMLInputElement }) => {
+    const canvas = within(canvasElement);
+
+    const skillInput = await canvas.findByRole('textbox');
+    const longInput = 'This is a long skill!';
+    await userEvent.type(skillInput, longInput, { delay: 100 });
+  },
+};
