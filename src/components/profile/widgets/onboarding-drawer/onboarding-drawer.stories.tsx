3230f6e90108c6e758d7f4afc1d15663d6d1a1c4
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-09-23 14:15:52 -0600

    [SRBT-256] Onboarding Drawer (#92)
    # [SRBT-256] Onboarding Drawer
    
    - Add `HandleInput` and `OnboardingDrawer` with stories 
    - Update radix dialog via shadcn cli
    - Adds `@storybook/preview-api` for seamless control of drawer in SB.
    
    # Screenshots
    
    Version 1:
    
    
    https://github.com/user-attachments/assets/773883e6-4795-4b65-8643-bfa0390a23f7

---
+++ src/components/profile/widgets/onboarding-drawer/onboarding-drawer.stories.tsx
@@ -0,0 +1,32 @@
+import { useArgs } from '@storybook/preview-api';
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import { OnboardingDrawer } from './onboarding-drawer';
+
+const meta: Meta<typeof OnboardingDrawer> = {
+  title: 'OnboardingDrawer',
+  component: OnboardingDrawer,
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    onSubmit: fn(),
+    open: true,
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof OnboardingDrawer>;
+
+export const Default: Story = {
+  render: (args) => {
+    // This is some SB boiler plate to sync the control with drawer state
+    const [{ open }, updateArgs] = useArgs();
+    const handleClose = () => {
+      updateArgs({ open: false });
+    };
+
+    return <OnboardingDrawer {...args} open={open} onClose={handleClose} />;
+  },
+};
