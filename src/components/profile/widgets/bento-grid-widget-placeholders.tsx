6dcc0652cd48e7aee8877e2d87bf49a34a4ded74
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-09-23 17:16:51 -0600

    Add onclick for grid

--- src/components/profile/widgets/bento-grid-widget-placeholders.tsx
+++ src/components/profile/widgets/bento-grid-widget-placeholders.tsx
@@ -2,38 +2,51 @@ import React from 'react';
 
 import { WidgetPlaceHolder } from './widget-placeholder';
 
-export const BentoGridWidgetPlaceholders: React.FC = () => {
+interface BentoGridWidgetPlaceholdersProps {
+  /** Callback for when any placeholder is clicked */
+  onClick?: () => void;
+}
+
+export const BentoGridWidgetPlaceholders: React.FC<
+  BentoGridWidgetPlaceholdersProps
+> = ({ onClick }) => {
   return (
     <div className='grid h-[720px] w-[1280px] grid-cols-4 grid-rows-2 gap-4 p-4'>
       <WidgetPlaceHolder
         type='TwitterProfile'
         size='A'
         className='col-span-1 row-span-1'
+        onClick={onClick}
       />
       <WidgetPlaceHolder
         type='Dribbble'
         size='D'
         className='col-span-1 row-span-2'
+        onClick={onClick}
       />
       <WidgetPlaceHolder
         type='Behance'
         size='A'
         className='col-span-2 row-span-1'
+        onClick={onClick}
       />
       <WidgetPlaceHolder
         type='InstagramProfile'
         size='A'
         className='col-span-1 row-span-1'
+        onClick={onClick}
       />
       <WidgetPlaceHolder
         type='InstagramProfile'
         size='A'
         className='col-span-1 row-span-1'
+        onClick={onClick}
       />
       <WidgetPlaceHolder
         type='SpotifySong'
         size='A'
         className='col-span-1 row-span-1'
+        onClick={onClick}
       />
     </div>
   );
