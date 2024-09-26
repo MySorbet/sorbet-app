96d0b02529818c2506bcc766b29d8d299ac17ed8
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-09-26 15:27:56 -0600

    🔥 Hotfix: Share Profile Icons (#99)
    # 🔥 Hotfix: Share Profile Icons
    
    Fix svg icon image src

--- src/components/profile/share/components/instagram-icon.tsx
+++ src/components/profile/share/components/instagram-icon.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
 
-import Instagram from '~/images/social/instagram.svg';
-
 export const InstagramIcon = () => {
-  return <Image src={Instagram} height={52} width={52} alt='Instagram logo' />;
+  return (
+    <Image
+      src='/images/social/instagram.svg'
+      height={52}
+      width={52}
+      alt='Instagram logo'
+    />
+  );
 };
--- src/components/profile/share/components/twitter-icon.tsx
+++ src/components/profile/share/components/twitter-icon.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
 
-import Twitter from '~/images/social/twitter.svg';
-
 export const TwitterIcon = () => {
-  return <Image src={Twitter} height={52} width={52} alt='Twitter logo' />;
+  return (
+    <Image
+      src='/images/social/twitter.svg'
+      height={52}
+      width={52}
+      alt='Twitter logo'
+    />
+  );
 };
