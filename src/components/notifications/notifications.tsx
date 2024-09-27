f4347e86e99e002513e7e8fd8a42e02c5da3e44a
Christopher Park <christopherjcpark00@gmail.com>
2024-09-27 14:20:07 -0700

    Feature: implements redirect when clicking on cell in notification feed

--- src/components/notifications/notifications.tsx
+++ src/components/notifications/notifications.tsx
@@ -6,6 +6,7 @@ import {
   NotificationFeedPopover,
   NotificationIconButton,
 } from '@knocklabs/react';
+import { useRouter } from 'next/navigation';
 import React, { useRef, useState } from 'react';
 
 import { NotificationToasts } from '@/components/notifications';
@@ -16,6 +17,7 @@ export const Notifications = () => {
   const [isVisible, setIsVisible] = useState(false);
   const buttonRef = useRef(null);
   const { user } = useAuth();
+  const router = useRouter();
 
   // No user, no notifications
   if (!user) {
@@ -37,6 +39,15 @@ export const Notifications = () => {
             buttonRef={buttonRef}
             isVisible={isVisible}
             onClose={() => setIsVisible(false)}
+            // TODO: Redirect to appropriate route based on 'item' data.
+            // TODO: The identifier would come from the backend, ideally relating to how we put state in the url
+            // TODO: i.e: if we put contract id in the url, the backend would populate the 'data' prop with the contract id
+            // For now, it is a simple redirect to 'gigs' page. As we have that more solidified, we can then have more detailed/specific redirects
+            onNotificationClick={(item) => {
+              console.log('item from onNotificationClick', item.data);
+              // TODO: look into why this works bc this isn't a client component at the moment
+              router.push('/gigs');
+            }}
           />
           <NotificationToasts />
         </KnockFeedProvider>
