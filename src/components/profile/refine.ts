d795869c22472f2e4dfa1a866dc789dc807fcac7
Christopher Park <christopherjcpark00@gmail.com>
2024-09-30 21:59:54 -0700

    Adds back comment to debounce requests

--- src/components/profile/refine.ts
+++ src/components/profile/refine.ts
@@ -1,5 +1,6 @@
-import { checkHandleIsAvailable } from "@/api/auth";
+import { checkHandleIsAvailable } from '@/api/auth';
 
+// TODO: debounce so that not too many requests are made when typing
 export const refine = async (handle: string, initialHandle: string) => {
   if (handle === initialHandle) return true; // initial handle generated for this user is allowed
   if (handle.length === 0) return false;
