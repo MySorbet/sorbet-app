da109d7a5c65d0ac73f337ce4299dc901ec2be89
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-10-03 17:40:44 -0600

    Remove unused and confusing period selector from wallet page Disable top up and send

--- src/app/wallet/wallet-balance.tsx
+++ src/app/wallet/wallet-balance.tsx
@@ -1,18 +1,27 @@
-import { SelectDuration } from '@/app/wallet/select-duration';
 import { Plus, Send, Wallet } from 'lucide-react';
 import React from 'react';
+
+import { Button } from '@/components/ui/button';
 
 interface WalletBalanceProps {
   ethBalance: string;
   usdcBalance: string;
+  onTopUp: () => void;
+  onSend: () => void;
 }
 
+/**
+ * Renders a users wallet balance and provides options to top up or send funds
+ * Currently, top up and send are disabled
+ */
 export const WalletBalance: React.FC<WalletBalanceProps> = ({
   ethBalance,
   usdcBalance,
+  onTopUp,
+  onSend,
 }) => {
   return (
-    <div className='min-h-[100%] rounded-3xl bg-white p-6 shadow-[0px_10px_30px_0px_#00000014]'>
+    <div className='min-h-[100%] min-w-80 rounded-3xl bg-white p-6 shadow-[0px_10px_30px_0px_#00000014]'>
       <div className='flex flex-col gap-1'>
         <div className='flex items-center justify-between'>
           <div className='flex flex-col gap-3'>
@@ -26,31 +35,42 @@ export const WalletBalance: React.FC<WalletBalanceProps> = ({
             </div>
           </div>
           <div className='flex gap-4'>
-            <button className='flex flex-col items-center gap-1 text-sm text-blue-500'>
-              <span className='flex h-12 w-12 items-center justify-center rounded-full bg-[#573DF5] text-white'>
-                <Plus size={26} />
-              </span>
-              <span className='text-[#595B5A]'>Top up</span>
-            </button>
-            <button className='flex flex-col items-center gap-1 text-sm text-blue-500'>
-              <span className='flex h-12 w-12 items-center justify-center rounded-full bg-[#573DF5] text-white'>
-                <Send size={26} />
-              </span>
-              <span className='text-[#595B5A]'>Send</span>
-            </button>
+            <CircleButton
+              icon={<Plus size={26} />}
+              label='Top up'
+              onClick={onTopUp}
+            />
+            <CircleButton
+              icon={<Send size={26} />}
+              label='Send'
+              onClick={onSend}
+            />
           </div>
         </div>
         <div className='flex'>
           {/* <div className='text-3xl font-semibold'>{ethBalance} ETH</div> */}
           <div className='text-3xl font-semibold'>{usdcBalance} USDC</div>
         </div>
-        <div className='mt-4'>
-          <SelectDuration
-            selectedValue='30'
-            onChange={(value: string) => console.log(value)}
-          />
-        </div>
       </div>
+    </div>
+  );
+};
+
+const CircleButton: React.FC<{
+  icon: React.ReactNode;
+  label?: string;
+  onClick?: () => void;
+}> = ({ icon, label, onClick }) => {
+  return (
+    <div className='flex h-auto w-auto flex-col items-center gap-1 p-0'>
+      <Button
+        className='bg-sorbet flex size-12 items-center justify-center rounded-full p-0 text-white'
+        onClick={onClick}
+        disabled // TODO: Enable when functionality is implemented
+      >
+        {icon}
+      </Button>
+      {label && <span className='text-sm text-[#595B5A]'>{label}</span>}
     </div>
   );
 };
