15b41fbfb459dadacbd6129e268feee298b9cdca
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-10-16 13:42:53 -0600

    optional step

--- src/app/invoices/components/create/create-invoice-header.tsx
+++ src/app/invoices/components/create/create-invoice-header.tsx
@@ -5,12 +5,12 @@ export const CreateInvoiceHeader = ({
   step,
 }: {
   children: React.ReactNode;
-  step: number;
+  step?: number;
 }) => {
   return (
     <div className='mb-4 flex items-center justify-between border-b border-gray-200 py-4'>
       <h2 className='text-2xl font-bold'>{children}</h2>
-      <Stepper step={step} totalSteps={3} />
+      {step && <Stepper step={step} totalSteps={3} />}
     </div>
   );
 };
