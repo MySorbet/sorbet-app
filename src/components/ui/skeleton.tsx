c43d056cfaf0c40d54b4057b41b2fc9890a0beb8
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-10-17 20:31:47 -0600

    Add lighter variant to skeleton

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,12 +1,31 @@
 import { cn } from "@/lib/utils"
 
+import { cva, type VariantProps } from "class-variance-authority"
+
+const skeletonVariants = cva(
+  'animate-pulse rounded-md bg-muted',
+  {
+    variants: {
+      variant: {
+        default: "",
+        lighter:
+          'bg-slate-200',
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+)
+
 function Skeleton({
   className,
+  variant,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof skeletonVariants>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn(skeletonVariants({ variant, className }))}
       {...props}
     />
   )
