923b494db883a65b03cdb64aa002561b5d84b042
Christopher Park <christopherjcpark00@gmail.com>
2024-10-18 23:07:25 -0700

    Updates fetch for usdc conversion to request usd conversion rate

--- src/app/wallet/USDCToUSDConversion.ts
+++ src/app/wallet/USDCToUSDConversion.ts
@@ -6,7 +6,7 @@ export const USDCToUSD = () => {
     queryFn: async () => {
       try {
         const response = await fetch(
-          'https://api.coingecko.com/api/v3/simple/price?ids=usd-coin&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true'
+          'https://api.coingecko.com/api/v3/simple/price?ids=usd-coin&vs_currencies=usd'
         );
         const data = await response.json();
         const rate = data['usd-coin'].usd;
