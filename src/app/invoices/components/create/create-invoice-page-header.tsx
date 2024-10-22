807766abe2102cd68a477ae196f2e1281d05de76
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-10-22 08:52:53 -0600

    create invoice header logo links to home

--- src/app/invoices/components/create/create-invoice-page-header.tsx
+++ src/app/invoices/components/create/create-invoice-page-header.tsx
@@ -1,8 +1,8 @@
 import { X } from '@untitled-ui/icons-react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
-
 type CreateInvoicePageHeaderProps = {
   onClose?: () => void;
 };
@@ -12,14 +12,16 @@ export const CreateInvoicePageHeader = ({
 }: CreateInvoicePageHeaderProps) => {
   return (
     <div className='flex items-center justify-between gap-3 px-8 py-4'>
-      <Image
-        src='/svg/logo.svg'
-        height={44}
-        width={44}
-        className='size-11'
-        alt='Sorbet logo'
-        priority
-      />
+      <Link href='/'>
+        <Image
+          src='/svg/logo.svg'
+          height={44}
+          width={44}
+          className='size-11'
+          alt='Sorbet logo'
+          priority
+        />
+      </Link>
       <Button variant='outline' className='h-fit p-2' onClick={onClose}>
         <X className='size-5' />
       </Button>
