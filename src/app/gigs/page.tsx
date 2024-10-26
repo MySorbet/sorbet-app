4577f5d5eb4143851fc7bb970b5017489b5596c1
Christopher Park <christopherjcpark00@gmail.com>
2024-10-25 17:15:36 -0700

    Tweaks icon sizes

--- src/app/gigs/page.tsx
+++ src/app/gigs/page.tsx
@@ -22,8 +22,8 @@ export default function GigsPage() {
       ) : (
         <div className='bg-sorbet/25 flex h-full w-full flex-col items-center justify-center'>
           <div className='relative flex size-3/4 items-center justify-center '>
-            <SorbetBlob className='absolute left-1/2 top-1/2 size-5/6 -translate-x-1/2 -translate-y-[60%] transform opacity-75' />
-            <h1 className='relative z-10 text-4xl text-white drop-shadow-md'>
+            <SorbetBlob className='absolute left-1/2 top-1/2 size-5/6 -translate-x-1/2 -translate-y-[50%] transform opacity-75 md:-translate-y-[60%]' />
+            <h1 className='relative z-10 text-2xl text-white drop-shadow-md md:text-4xl'>
               Gigs are coming back soon!
             </h1>
           </div>
@@ -38,6 +38,7 @@ export default function GigsPage() {
                 height={52}
                 width={52}
                 alt='Sorbet logo'
+                className='size-10'
               />
             </a>
             <a
@@ -45,12 +46,13 @@ export default function GigsPage() {
               target='_blank'
               rel='noopener noreferrer'
             >
-              <InstagramIcon />
+              <InstagramIcon className='size-10' />
             </a>
             <a
               href='https://x.com/mysorbetxyz'
               target='_blank'
               rel='noopener noreferrer'
+              className='size-10'
             >
               <TwitterIcon />
             </a>
--- src/components/profile/share/components/twitter-icon.tsx
+++ src/components/profile/share/components/twitter-icon.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 
-export const TwitterIcon = () => {
+export const TwitterIcon = ({ className }: { className?: string }) => {
   return (
     <Image
       src='/images/social/twitter.svg'
       height={52}
       width={52}
       alt='Twitter logo'
+      className={className}
     />
   );
 };
