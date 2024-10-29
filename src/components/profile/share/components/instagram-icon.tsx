fb83bad23d7a9e8075381df16372470d0f87718a
Christopher Park <113001736+ChristopherPark3@users.noreply.github.com>
2024-10-29 13:53:09 -0700

    [SRBT-316] Disable gigs (#120)
    # [SRBT-316] Disable gigs
    
    **Changes**
    - Creates feature flag object to indicate feature statuses
    - Removes gigs from sidebar
    - Disables redirect from toasts and Knock notification popover (opens
    when bell icon is clicked)
    - Creates a temporary under dev page for gigs
    
    # Screenshots
    
    ![Screenshot 2024-10-25 at 5 23
    15 PM](https://github.com/user-attachments/assets/47cb9496-8ed9-4c9f-a1ba-3694fec35a18)

--- src/components/profile/share/components/instagram-icon.tsx
+++ src/components/profile/share/components/instagram-icon.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 
-export const InstagramIcon = () => {
+export const InstagramIcon = ({ className }: { className?: string }) => {
   return (
     <Image
       src='/images/social/instagram.svg'
       height={52}
       width={52}
       alt='Instagram logo'
+      className={className}
     />
   );
 };
