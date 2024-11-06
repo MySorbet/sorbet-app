2759d4abf5cf2c81d33907f6e5806dba2e26e0c8
Christopher Park <christopherjcpark00@gmail.com>
2024-11-06 11:23:01 -0800

    Adds comment for duration

--- src/constant/constants.ts
+++ src/constant/constants.ts
@@ -4,4 +4,4 @@ export const MAX_NUM_SKILLS = 5;
 export const MAX_CHARS_PER_SKILL = 20;
 
 /** PostHog */
-export const SESSION_REPLAY_DURATION = 5 * 60 * 1000;
+export const SESSION_REPLAY_DURATION = 5 * 60 * 1000; // 5 minutes
