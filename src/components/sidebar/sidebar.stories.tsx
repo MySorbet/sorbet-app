c6a9de9925999e2cde4195d9047f24b5f2bc5c6e
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-11-08 11:46:24 -0700

    Update sidebar stories with useArgs and open button

--- src/components/sidebar/sidebar.stories.tsx
+++ src/components/sidebar/sidebar.stories.tsx
@@ -2,6 +2,7 @@ import { useArgs } from '@storybook/preview-api';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Sidebar } from './sidebar';
+import { Button } from '@/components/ui/button';
 
 const meta = {
   title: 'Sidebar',
@@ -24,4 +25,19 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof Sidebar>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  render: (args) => {
+    const [{ isOpen }, updateArgs] = useArgs();
+    const handleSetOpen = (isOpen: boolean) => {
+      updateArgs({ isOpen });
+    };
+    return (
+      <>
+        <Sidebar {...args} isOpen={isOpen} onIsOpenChange={handleSetOpen} />
+        <Button variant='sorbet' onClick={() => handleSetOpen(!isOpen)}>
+          Open sidebar
+        </Button>
+      </>
+    );
+  },
+};
