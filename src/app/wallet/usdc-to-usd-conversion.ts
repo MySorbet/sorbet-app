bb863a9fe673405bd2ddfeb32e27e3cf9877b581
Christopher Park <christopherjcpark00@gmail.com>
2024-11-12 14:08:15 -0800

    Also fixes weird conversion bug that was happening on prod

--- src/app/wallet/usdc-to-usd-conversion.ts
+++ src/app/wallet/usdc-to-usd-conversion.ts
@@ -7,18 +7,19 @@ export const USDCToUSD = () => {
   return useQuery({
     queryKey: ['USDCToUSDConversionRate'],
     queryFn: async () => {
-      try {
-        const response = await fetch(
-          'https://api.coingecko.com/api/v3/simple/price?ids=usd-coin&vs_currencies=usd'
-        );
-        const data = await response.json();
-        const rate = data['usd-coin'].usd;
-        return rate;
-      } catch (error: unknown) {
-        console.error('Error fetching USDC to USD conversion rate:', error);
-        return 1;
+      if (!featureFlags.coinGeckoApi) {
+        try {
+          const response = await fetch(
+            'https://api.coingecko.com/api/v3/simple/price?ids=usd-coin&vs_currencies=usd'
+          );
+          const data = await response.json();
+          const rate = data['usd-coin'].usd;
+          return rate;
+        } catch (error: unknown) {
+          console.error('Error fetching USDC to USD conversion rate:', error);
+          return 1;
+        }
       }
     },
-    enabled: featureFlags.coinGeckoApi,
   });
 };
