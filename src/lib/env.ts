3aaea293f7992ef119e97b208357aa188d434ff8
Dillon Cutaiar <dmcutaiar@gmail.com>
2024-11-14 17:11:37 -0700

    Update `NEXT_PUBLIC_BASESEPOLIA_EXPLORER` to `NEXT_PUBLIC_BASE_EXPLORER`

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -12,7 +12,7 @@ export const env = createEnv({
     NEXT_PUBLIC_PRIVY_APP_ID: z.string(),
     NEXT_PUBLIC_BASE_CONTRACT_ADDRESS: z.string(),
     NEXT_PUBLIC_BASE_USDC_ADDRESS: z.string(),
-    NEXT_PUBLIC_BASESEPOLIA_EXPLORER: z.string().url(),
+    NEXT_PUBLIC_BASE_EXPLORER: z.string().url(),
     NEXT_PUBLIC_BASE_RPC_URL: z.string(),
     NEXT_PUBLIC_POSTHOG_KEY: z.string(),
     NEXT_PUBLIC_POSTHOG_HOST: z.string().url(),
@@ -30,11 +30,10 @@ export const env = createEnv({
     NEXT_PUBLIC_BASE_CONTRACT_ADDRESS:
       process.env.NEXT_PUBLIC_BASE_CONTRACT_ADDRESS,
     NEXT_PUBLIC_BASE_USDC_ADDRESS: process.env.NEXT_PUBLIC_BASE_USDC_ADDRESS,
-    NEXT_PUBLIC_BASESEPOLIA_EXPLORER:
-      process.env.NEXT_PUBLIC_BASESEPOLIA_EXPLORER,
+    NEXT_PUBLIC_BASE_EXPLORER: process.env.NEXT_PUBLIC_BASE_EXPLORER,
     NEXT_PUBLIC_BASE_RPC_URL: process.env.NEXT_PUBLIC_BASE_RPC_URL,
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
-    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST
+    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   },
   // Why? https://env.t3.gg/docs/customization#treat-empty-strings-as-undefined
   emptyStringAsUndefined: true,
