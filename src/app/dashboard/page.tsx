a8253472fdaea766021d958b8ec7a9cee2fb669d
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-01-07 15:16:23 -0700

    wrap dashboard in authenticated

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,17 @@
+import Authenticated from '@/app/authenticated';
 import { Header } from '@/components/header';
 
 import { Dashboard } from './components/dashboard';
 
 export default function DashboardPage() {
   return (
-    <main className='flex size-full flex-col'>
-      <Header />
-      <div className='container flex flex-1 justify-center pt-14'>
-        <Dashboard />
-      </div>
-    </main>
+    <Authenticated>
+      <main className='flex size-full flex-col'>
+        <Header />
+        <div className='container flex flex-1 justify-center pt-14'>
+          <Dashboard />
+        </div>
+      </main>
+    </Authenticated>
   );
 }
