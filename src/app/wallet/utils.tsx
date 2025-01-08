c464f014248f6e5935c26d617caf582a86e9609d
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-01-08 14:39:01 -0700

    extract format currency to project scope

--- src/app/wallet/utils.tsx
+++ src/app/wallet/utils.tsx
@@ -2,6 +2,7 @@ export const formatWalletAddress = (account: string): string => {
   return `${account.slice(0, 5)}...${account.slice(-5)}`;
 };
 
+// TODO: Merge this with the one in @/lib/currency.ts?
 export const formatCurrency = (currency: string): string => {
   return Number(currency).toLocaleString();
 };
