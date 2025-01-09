8a7200edf4f02284908d55832358ad02a141e0e5
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-01-09 11:37:15 -0700

    Open profile edit from dashboard

--- src/app/dashboard/components/dashboard.tsx
+++ src/app/dashboard/components/dashboard.tsx
@@ -3,9 +3,11 @@
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
+import { ProfileEditModal } from '@/components/profile/profile-edit-modal';
 import { useSmartWalletAddress, useWalletBalances } from '@/hooks';
 import { useAuth } from '@/hooks/use-auth';
 import { useIsMobile } from '@/hooks/use-is-mobile';
+import { User } from '@/types';
 
 import { useDashboardData } from '../hooks/use-dashboard-data';
 import { type TaskType, ChecklistCard } from './checklist-card';
@@ -13,10 +15,25 @@ import { OpenOnDesktopDrawer } from './open-on-desktop-drawer';
 import { type StatsCardType, StatsCard } from './stats-card';
 import { WelcomeCard } from './welcome-card';
 
+/**
+ * Composes dashboard cards into a fluid layout
+ * Also manages state, click actions, and routing actions
+ * TODO: What if this component stayed server side and just managed layout. And the former responsibilities were hoisted?
+ */
 export const Dashboard = () => {
   const [open, setOpen] = useState(false);
   const isMobile = useIsMobile();
   const router = useRouter();
+  const { data } = useDashboardData();
+  const { user } = useAuth();
+
+  // TODO: Think about who should format the balance
+  const { smartWalletAddress: walletAddress } = useSmartWalletAddress();
+  const { usdcBalance, loading: isBalanceLoading } =
+    useWalletBalances(walletAddress);
+
+  const [isProfileEditModalOpen, setIsProfileEditModalOpen] = useState(false);
+
   const handleCardClicked = (type: StatsCardType | TaskType) => {
     if (isMobile) {
       setOpen(true);
@@ -41,8 +58,7 @@ export const Dashboard = () => {
           router.push('/invoices/create');
           break;
         case 'profile':
-          // TODO: Open profile edit modal instead
-          router.push(`/${user?.handle}`);
+          setIsProfileEditModalOpen(true);
           break;
         case 'widget':
           router.push(`/${user?.handle}?drawerOpen=true`);
@@ -58,19 +74,19 @@ export const Dashboard = () => {
     }
   };
 
-  // TODO should this go here or 1 level up?
-  const { data } = useDashboardData();
-  const { user } = useAuth();
-
-  // TODO: Think about who should format the balance
-  const { smartWalletAddress: walletAddress } = useSmartWalletAddress();
-  const { usdcBalance, loading: isBalanceLoading } =
-    useWalletBalances(walletAddress);
-
   return (
     <>
       {/* Conditionally rendered drawer for mobile clicks */}
       <OpenOnDesktopDrawer open={open} onClose={() => setOpen(false)} />
+
+      {/* Conditionally rendered profile edit modal */}
+      {user && (
+        <ProfileEditModal
+          editModalVisible={isProfileEditModalOpen}
+          handleModalVisible={setIsProfileEditModalOpen}
+          user={user as User} // TODO: Fix User typing to remove cast
+        />
+      )}
 
       {/* Fluid dashboard layout */}
       <div className='@container @lg:grid-cols-[minmax(0,1fr),300px] grid h-fit w-full max-w-5xl grid-cols-1 gap-4'>
