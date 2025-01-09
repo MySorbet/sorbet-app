e697cb65e96910fb916cad7779cb5d2545881f81
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-01-09 16:28:33 -0700

    bottom pad

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,7 +8,7 @@ export default function DashboardPage() {
     <Authenticated>
       <main className='flex size-full flex-col'>
         <Header />
-        <div className='container flex flex-1 justify-center pt-6'>
+        <div className='container flex flex-1 justify-center py-6'>
           <Dashboard />
         </div>
       </main>
