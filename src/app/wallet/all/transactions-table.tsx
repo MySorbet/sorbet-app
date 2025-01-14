46bc9d1fdabce61f0edcd6ff8bea3fbe18d4311b
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-01-13 18:08:24 -0700

    Allow copying transactions

--- src/app/wallet/all/transactions-table.tsx
+++ src/app/wallet/all/transactions-table.tsx
@@ -1,6 +1,6 @@
 import { LinkExternal02 } from '@untitled-ui/icons-react';
 import { ArrowDown, ChevronDown, Plus, Search, Send } from 'lucide-react';
-import React from 'react';
+import React, { useState } from 'react';
 import { DateRange } from 'react-day-picker';
 
 import {
@@ -8,10 +8,16 @@ import {
   formatTransactionDate,
   formatWalletAddress,
 } from '@/app/wallet/utils';
-import { Spinner } from '@/components/common';
+import useCopy from '@/components/common/copy-button/use-copy';
 import { DatePickerWithRange } from '@/components/ui/date-range-picker';
-import { env } from '@/lib/env';
 import { Skeleton } from '@/components/ui/skeleton';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { env } from '@/lib/env';
 
 export interface TableTransaction {
   account: string;
@@ -136,9 +142,7 @@ const TransactionsTable: React.FC<TransactionsTableProps> = ({
                           </span>
                         </div>
                         <div className='ml-4'>
-                          <div className='text-sm font-medium text-gray-900'>
-                            {formatWalletAddress(transaction.account)}
-                          </div>
+                          <AddressText address={transaction.account} />
                           <div className='mt-1 text-xs text-[#595B5A]'>
                             {transaction.type === 'Self-transfer'
                               ? 'Added'
@@ -175,3 +179,42 @@ const TransactionsTable: React.FC<TransactionsTableProps> = ({
 };
 
 export default TransactionsTable;
+
+// Tooltip state inspired by https://github.com/shadcn-ui/ui/issues/86#issuecomment-2241817826
+// TODO: Revisit "copy" flash when tooltip is closing (likely due to fade out animation)
+/** Local component to display formatted address and allow copying */
+const AddressText = ({ address }: { address: string }) => {
+  const formattedAddress = formatWalletAddress(address);
+  const { isCopied, handleClick } = useCopy(address);
+  const [open, setOpen] = useState(false);
+
+  return (
+    <TooltipProvider>
+      <Tooltip open={open || isCopied}>
+        <TooltipTrigger asChild>
+          <button
+            type='button'
+            className='cursor-pointer'
+            onClick={() => {
+              setOpen(!open);
+              handleClick();
+            }}
+            onMouseEnter={() => setOpen(true)}
+            onMouseLeave={() => setOpen(false)}
+            onTouchStart={() => setOpen(!open)}
+            onKeyDown={(e) => {
+              e.preventDefault();
+              if (e.key === 'Enter') {
+                setOpen(!open);
+                handleClick();
+              }
+            }}
+          >
+            {formattedAddress}
+          </button>
+        </TooltipTrigger>
+        <TooltipContent>{isCopied ? 'Copied!' : 'Copy'}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
