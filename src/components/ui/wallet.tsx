21308b800102f74d34e6c7c24b5cef3532873399
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-02-14 20:15:50 -0700

    use more subtle wallet animation

--- src/components/ui/wallet.tsx
+++ src/components/ui/wallet.tsx
@@ -10,25 +10,25 @@ export interface WalletIconHandle {
   stopAnimation: () => void;
 }
 
-const pathVariant: Variants = {
-  normal: { pathLength: 1, opacity: 1, pathOffset: 0 },
-  animate: {
-    pathLength: [0, 1],
-    opacity: [0, 1],
-    pathOffset: [1, 0],
-  },
-};
-
-const secondPathVariant: Variants = {
+const iconVariants: Variants = {
   normal: {
-    pathLength: 1,
-    pathOffset: 0,
     scale: 1,
+    rotate: 0,
   },
   animate: {
-    pathLength: [0, 1],
-    pathOffset: [1, 0],
-    scale: [0.5, 1],
+    scale: 1.05,
+    rotate: [0, -7, 7, 0],
+    transition: {
+      rotate: {
+        duration: 0.5,
+        ease: 'easeInOut',
+      },
+      scale: {
+        type: 'spring',
+        stiffness: 400,
+        damping: 10,
+      },
+    },
   },
 };
 
@@ -74,7 +74,7 @@ const WalletIcon = forwardRef<WalletIconHandle, HTMLAttributes<HTMLDivElement>>(
         onMouseLeave={handleMouseLeave}
         {...props}
       >
-        <svg
+        <motion.svg
           xmlns='http://www.w3.org/2000/svg'
           width='24'
           height='24'
@@ -84,22 +84,16 @@ const WalletIcon = forwardRef<WalletIconHandle, HTMLAttributes<HTMLDivElement>>(
           strokeWidth='2'
           strokeLinecap='round'
           strokeLinejoin='round'
+          variants={iconVariants}
+          animate={controls}
         >
           <motion.path
-            variants={secondPathVariant}
-            animate={controls}
-            transition={{
-              delay: 0.2,
-              duration: 0.4,
-            }}
             d='M19 7V4a1 1 0 0 0-1-1H5a2 2 0 0 0 0 4h15a1 1 0 0 1 1 1v4h-3a2 2 0 0 0 0 4h3a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1'
           />
           <motion.path
-            variants={pathVariant}
-            animate={controls}
             d='M3 5v14a2 2 0 0 0 2 2h15a1 1 0 0 0 1-1v-4'
           />
-        </svg>
+        </motion.svg>
       </div>
     );
   }
