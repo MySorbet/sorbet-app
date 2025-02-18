ced207da88eecbfa439d4a5afe8cdee642b33c65
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-02-18 13:48:25 -0700

    Add items card

---
+++ src/app/invoices/v2/items-card.stories.tsx
@@ -0,0 +1,78 @@
+import { type Meta, type StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+import { ItemsCard } from './items-card';
+
+const meta = {
+  title: 'Invoices/v2/ItemsCard',
+  component: ItemsCard,
+  parameters: {
+    layout: 'centered',
+  },
+  args: {
+    items: [
+      {
+        name: 'Website Development',
+        quantity: 1,
+        amount: 1000,
+      },
+      {
+        name: 'UI/UX Design',
+        quantity: 2,
+        amount: 500,
+      },
+    ],
+    onItemsChange: fn(),
+  },
+} satisfies Meta<typeof ItemsCard>;
+
+export default meta;
+type Story = StoryObj<typeof ItemsCard>;
+
+export const Default: Story = {};
+
+export const Empty: Story = {
+  args: {
+    items: [
+      {
+        name: '',
+        quantity: 0,
+        amount: 0,
+      },
+    ],
+  },
+};
+
+export const SingleItem: Story = {
+  args: {
+    items: [
+      {
+        name: 'Consulting Services',
+        quantity: 1,
+        amount: 750,
+      },
+    ],
+  },
+};
+
+export const MultipleItems: Story = {
+  args: {
+    items: [
+      {
+        name: 'Web Development',
+        quantity: 1,
+        amount: 2000,
+      },
+      {
+        name: 'Content Writing',
+        quantity: 3,
+        amount: 300,
+      },
+      {
+        name: 'SEO Optimization',
+        quantity: 1,
+        amount: 500,
+      },
+    ],
+  },
+};
---
+++ src/app/invoices/v2/schema.ts
@@ -0,0 +1,36 @@
+import { z } from 'zod';
+
+// TODO: Note there are duplicates of v1 invoice stuff here. At the end of the day, there should only be one
+/**
+ * Build a zod string validator with a custom error message for use in a RHF FormField
+ *
+ * @param name - The name of the field
+ * @param min - The minimum length of the field (default: 1)
+ * @param max - The maximum length of the field (default: 50)
+ * @returns A zod string validator enforcing the above constraints
+ */
+export const invoiceFormStringValidator = (name: string, min = 1, max = 50) =>
+  z
+    .string({
+      errorMap: () => ({
+        message: `${name} must be between ${min} and ${max} characters`,
+      }),
+    })
+    .min(min)
+    .max(max);
+
+/** Validator for the data of an invoice item */
+const InvoiceItemDataSchema = z.object({
+  name: invoiceFormStringValidator('Item name'),
+  quantity: z.coerce.number().min(1),
+  amount: z.coerce.number().min(0),
+});
+/** Type of the data of an invoice item */
+export type InvoiceItemData = z.infer<typeof InvoiceItemDataSchema>;
+
+/** Empty invoice item data */
+export const emptyInvoiceItemData: InvoiceItemData = {
+  name: '',
+  quantity: 1,
+  amount: 0,
+};
