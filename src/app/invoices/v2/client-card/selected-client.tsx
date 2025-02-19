b4436bfe4381e83d0f914eccdef93c709220712f
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-02-19 14:59:46 -0700

    Update selected client style

--- src/app/invoices/v2/client-card/selected-client.tsx
+++ src/app/invoices/v2/client-card/selected-client.tsx
@@ -18,18 +18,28 @@ export const SelectedClient = ({
 }) => {
   return (
     <div className='flex items-center gap-2 p-2'>
-      <SquareUser className='size-8' />
+      <SquareUser className='size-8' strokeWidth={1.5} />
 
-      <div>
-        <p className='text-sm font-semibold'>{client.name}</p>
-        <p className='text-xs'>{client.email}</p>
+      <div className='mr-4'>
+        <p className='text-sm font-semibold leading-none'>{client.name}</p>
+        <p className='text-xs leading-none'>{client.email}</p>
       </div>
 
-      <Button variant='ghost' size='icon' onClick={onEdit}>
+      <Button
+        variant='ghost'
+        size='icon'
+        className='text-muted-foreground size-6 p-1.5'
+        onClick={onEdit}
+      >
         <FilePenLine className='size-6' />
       </Button>
 
-      <Button variant='ghost' size='icon' onClick={onDelete}>
+      <Button
+        variant='ghost'
+        size='icon'
+        className='text-muted-foreground size-6 p-1.5'
+        onClick={onDelete}
+      >
         <X className='size-6' />
       </Button>
     </div>
