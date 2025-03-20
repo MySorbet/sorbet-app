713fb22d493c2d88d9814b08e9b655ed47e26534
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-03-19 18:16:17 -0600

    Update profile og images with proper font and svg loading

--- src/app/[handle]/opengraph-image.tsx
+++ src/app/[handle]/opengraph-image.tsx
@@ -1,4 +1,7 @@
 import { ImageResponse } from 'next/og';
+import type { ImageResponseOptions } from 'next/server';
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
 
 import { env } from '@/lib/env';
 import { formatName } from '@/lib/utils';
@@ -6,7 +9,7 @@ import { formatName } from '@/lib/utils';
 const API_URL = env.NEXT_PUBLIC_SORBET_API_URL;
 
 // TODO: How to customize the alt text to be their handle?
-export const alt = 'Freelancer';
+export const alt = 'Sorbet Profile';
 
 // Standard size for opengraph images
 export const size = {
@@ -26,8 +29,12 @@ export default async function Image({
     cache: 'no-store', // No store because we need to get the latest user
   }).then((res) => res.json());
 
-  // TODO: Self host inter and use it here. This should fix the fontWeights not being right
   const name = formatName(user.firstName, user.lastName) ?? user.handle;
+
+  const avatarFallbackSrc = await createSVGBase64(
+    'public/svg/avatar-fallback.svg'
+  );
+  const logoSrc = await createSVGBase64('public/svg/logo.svg');
 
   return new ImageResponse(
     (
@@ -56,48 +63,13 @@ export default async function Image({
             alignItems: 'center',
           }}
         >
-          {/* TODO: could we read the sorbet logo from file rather than duplicating it here? */}
-          <svg
-            width='35'
-            height='36'
-            viewBox='0 0 35 36'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <rect
-              y='0.913086'
-              width='34.1747'
-              height='34.1747'
-              rx='17.0873'
-              fill='#6230EC'
-            />
-            <path
-              d='M15.7476 8.79731C15.7476 8.57983 15.5543 8.41228 15.3411 8.45506C12.3039 9.06438 10.0348 11.4873 10.0348 14.3835C10.0348 17.2796 12.3039 19.7026 15.3411 20.3119C15.5543 20.3547 15.7476 20.1871 15.7476 19.9696L15.7476 8.79731Z'
-              fill='#D3EC30'
-            />
-            <path
-              fill-rule='evenodd'
-              clip-rule='evenodd'
-              d='M21.1325 8.38529C21.1478 8.38505 21.1632 8.38492 21.1787 8.38492C22.7562 8.38492 24.035 9.66376 24.035 11.2413C24.035 12.8188 22.7562 14.0977 21.1787 14.0977C19.9798 14.0977 18.9534 13.359 18.5297 12.312C18.1556 11.8719 17.8446 11.6934 17.1831 11.4768C16.9617 11.4043 16.7419 11.3129 16.6945 11.2737C16.6472 11.2346 16.6085 11.1941 16.6085 11.1838C16.6085 11.0495 16.7638 10.9458 17.166 10.8116C17.8813 10.573 18.276 10.3229 18.5824 9.91409C18.7254 9.72337 18.8551 9.56071 18.978 9.42019C19.0644 9.31587 19.1581 9.2178 19.2583 9.12675C19.4754 8.92208 19.6859 8.77861 19.9316 8.65748C20.3221 8.46494 20.7315 8.3791 21.1325 8.38529Z'
-              fill='#D3EC30'
-            />
-            <path
-              d='M18.3221 27.2056C18.3221 27.4231 18.5154 27.5906 18.7286 27.5479C21.7658 26.9386 24.0349 24.5156 24.0349 21.6195C24.0349 18.7233 21.7658 16.3004 18.7286 15.6911C18.5154 15.6483 18.3221 15.8158 18.3221 16.0333L18.3221 27.2056Z'
-              fill='#D3EC30'
-            />
-            <path
-              fill-rule='evenodd'
-              clip-rule='evenodd'
-              d='M12.9365 27.6177C12.9214 27.6179 12.9063 27.618 12.8912 27.618C11.3136 27.618 10.0348 26.3392 10.0348 24.7616C10.0348 23.1841 11.3136 21.9053 12.8912 21.9053C14.0901 21.9053 15.1165 22.6439 15.5401 23.691C15.9142 24.1311 16.2253 24.3096 16.8868 24.5262C17.1081 24.5986 17.328 24.69 17.3753 24.7292C17.4226 24.7684 17.4614 24.8089 17.4614 24.8191C17.4614 24.9535 17.306 25.0572 16.9038 25.1914C16.1886 25.43 15.7939 25.6801 15.4874 26.0889C15.3441 26.28 15.2141 26.443 15.091 26.5838C15.0052 26.6872 14.9123 26.7845 14.8129 26.8749C14.5954 27.0802 14.3845 27.2241 14.1383 27.3455C13.7475 27.5382 13.3377 27.624 12.9365 27.6177Z'
-              fill='#D3EC30'
-            />
-          </svg>
+          <img src={logoSrc} alt='Sorbet Logo' />
           <span
             style={{
               color: '#6230EC',
-              fontSize: 14,
+              fontSize: 18,
               fontWeight: 600,
-              letterSpacing: 0.025,
+              letterSpacing: 1,
             }}
           >
             SORBET
@@ -114,36 +86,22 @@ export default async function Image({
             borderRadius: '100%',
           }}
         >
-          {user.profileImage ? (
-            <img
-              src={user.profileImage}
-              alt={user.firstName}
-              style={{
-                width: '100%',
-                height: '100%',
-                objectFit: 'cover',
-                borderRadius: '100%',
-              }}
-            />
-          ) : (
-            // TODO: Use the default avatar at avatar-fallback.svg instead
-            <div
-              style={{
-                width: '100%',
-                height: '100%',
-                objectFit: 'cover',
-                borderRadius: '100%',
-                backgroundColor: '#e5e7eb',
-                display: 'flex',
-              }}
-            />
-          )}
+          <img
+            src={user.profileImage || avatarFallbackSrc}
+            alt={user.firstName}
+            style={{
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              borderRadius: '100%',
+            }}
+          />
         </span>
         <div style={{ display: 'flex', flexDirection: 'column' }}>
           <span
             style={{
               fontSize: 60,
-              fontWeight: 700,
+              fontWeight: 600,
               lineHeight: 1,
             }}
           >
@@ -154,6 +112,43 @@ export default async function Image({
     ),
     {
       ...size,
+      fonts: await getLocalFonts(),
     }
   );
+}
+
+/**
+ * Read an svg from the project (relative to the project root) and return a base64 encoded string (to pass to an image tag)
+ * - loosely based on https://github.com/vercel/satori/issues/311#issuecomment-2294864621
+ */
+const createSVGBase64 = async (url: string) => {
+  const buffer = Buffer.from(await readFile(join(process.cwd(), url)));
+  return `data:${'image/svg+xml'};base64,${buffer.toString('base64')}`;
+};
+
+/**
+ * We would like to use Inter for the opengraph image.
+ * - Loading to from google fonts is slow
+ * - We could self host the variable font and use that here, but variable fonts seem not to work in satori yet
+ * - So we just host a 600 weight static font specifically for this og image
+ *
+ * Reference
+ * - https://nextjs.org/docs/app/api-reference/file-conventions/metadata/opengraph-image#generate-images-using-code-js-ts-tsx
+ * - https://github.com/vercel/next.js/discussions/49860
+ * - https://github.com/vercel/next.js/issues/48081
+ */
+export async function getLocalFonts(): Promise<ImageResponseOptions['fonts']> {
+  const fontData = await readFile(
+    join(process.cwd(), 'public/fonts/Inter_18pt-SemiBold.ttf')
+  );
+  const fontSrc = Uint8Array.from(fontData).buffer;
+
+  return [
+    {
+      name: `Inter`,
+      data: fontSrc,
+      style: 'normal',
+      weight: 600,
+    },
+  ];
 }
