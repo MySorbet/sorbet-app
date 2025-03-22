afc323c2995f12ea311be7d19fe8c49f51f1ca7a
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-03-21 20:28:23 -0600

    fix build

--- src/app/v2/[handle]/components/widget/grid.stories.tsx
+++ src/app/v2/[handle]/components/widget/grid.stories.tsx
@@ -1,11 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { useState } from 'react';
 
 import { Button } from '@/components/ui/button';
 import { sleep } from '@/lib/utils';
 
 import { WidgetGrid } from './grid';
-import { ramiMockWidgets } from './mock-widgets';
 import { useWidgets, WidgetProvider } from './use-widget-context';
 
 const meta = {
@@ -52,45 +50,4 @@ export const WithAddButton: Story = {
 const mockFetchWidgetData = async (url: string) => {
   await sleep(3000);
   return { contentUrl: url, iconUrl: url };
-};
-
-export const RenderedDeprecated = {
-  render: () => {
-    const [widgets, setWidgets] = useState(ramiMockWidgets);
-
-    return (
-      <div className='size-full'>
-        <Button
-          onClick={async () => {
-            const newWidgets = [
-              {
-                id: '5',
-                title: 'picsum.photos',
-                href: 'https://picsum.photos/200/300',
-                size: 'C' as const,
-                loading: true,
-              },
-              ...Object.values(ramiMockWidgets),
-            ];
-            setWidgets(newWidgets);
-            const data = await mockFetchWidgetData(
-              'https://picsum.photos/200/300'
-            );
-            // Find widget with id 5 and update it imperatively
-            const widget = newWidgets.find((w) => w.id === '5');
-            if (widget) {
-              console.log('widget', widget);
-              setWidgets([
-                { ...widget, loading: false, ...data, title: 'Something nice' },
-                ...newWidgets.filter((w) => w.id !== '5'),
-              ]);
-            }
-          }}
-        >
-          Add widget
-        </Button>
-        <WidgetGrid widgets={widgets} />
-      </div>
-    );
-  },
 };
