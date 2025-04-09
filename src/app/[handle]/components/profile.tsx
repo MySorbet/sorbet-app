f55cd6cbe018dac4fc5f3335cc21b7978caa75e4
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-04-09 15:48:46 -0600

    First attempt at an animated force mobile view

--- src/app/[handle]/components/profile.tsx
+++ src/app/[handle]/components/profile.tsx
@@ -58,113 +58,147 @@ export const Profile = ({
 
   useHandlePaste(addWidget, isMine && !isLoading);
 
-  // This hook fires at 768px, aligning perfectly with our @3xl breakpoint below
-  const isMobile = useIsMobile();
+  // This hook fires at 768px, which we will consider the user to be on a mobile device
+  const disableControlBar = useIsMobile();
+
+  // Shrink the parent container to simulate a mobile view
+  const [isArtificialMobile, setIsArtificialMobile] = useState(false);
 
   return (
-    <div className='@container size-full'>
-      <div className='@3xl:flex-row @3xl:overflow-y-visible flex size-full flex-col items-center overflow-y-auto'>
-        {/* Left part of the profile. desktop: full height and long enough to render profile details in desktop mode. */}
-        {/* mobile: auto height and short enough to render profile details in mobile mode. */}
-        <div
-          className={cn(
-            '@3xl:h-full @3xl:min-w-96 flex w-[328px] flex-col justify-between gap-6 p-6',
-            'animate-in fade-in-0 duration-500'
-          )}
+    <div
+      className={cn(
+        'size-full transition-all duration-300',
+        isArtificialMobile && 'bg-muted/70 py-4 pb-28'
+      )}
+    >
+      {/* This is the container which forces a mobile view or not */}
+      <motion.div
+        className={cn(
+          '@container bg-background relative mx-auto size-full',
+          isArtificialMobile &&
+            'border-muted w-[448px] overflow-clip rounded-3xl border-2 shadow-md'
+        )}
+        initial={{ width: '100%' }}
+        animate={{
+          width: isArtificialMobile ? '448px' : '100%',
+        }}
+        transition={{ duration: 0.3, ease: 'easeOut' }}
+      >
+        {/* Main row or col of the profile. */}
+        <motion.div
+          className='@[81rem]:flex-row @[81rem]:overflow-y-visible flex size-full flex-col items-center overflow-y-auto'
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: isArtificialMobile ? [1, 0, 0, 1] : 1, // Apply the opacity trick
+          }}
+          transition={{
+            duration: 1, // Total duration
+            times: [0, 0, 0.7, 1], // Quick fade-in, hold, and fade-out
+          }}
         >
-          <ProfileDetails
-            user={user}
-            isMine={isMine}
-            onEdit={() => setIsEditing(true)}
-            onContactMe={() => setIsContactMeDialogOpen(true)}
-          />
-          <ExitLinks
-            isLoggedIn={isLoggedIn}
-            isMine={isMine}
-            className='@3xl:flex hidden'
-          />
-        </div>
-        {/* The right side of the profile. Should handle scroll itself (except on mobile, where the whole page will scroll*/}
-        <div className='@3xl:w-auto @3xl:h-full @3xl:overflow-y-auto @3xl:flex-1 w-full'>
-          {showOnboarding ? (
-            <div className='flex h-full w-full items-center justify-center'>
-              <OnboardWithHandles onSubmit={handleAddMultipleWidgets} />
-            </div>
-          ) : (
-            <>
-              <motion.div
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1, delay: 0.5, ease: 'easeOut' }}
-                className='@3xl:mb-20' // Little hack to make sure control bar doesn't overlap the bottommost widget controls
-              >
-                <ErrorBoundary FallbackComponent={GridErrorFallback}>
-                  <WidgetGrid immutable={!isMine || isMobile} />
-                </ErrorBoundary>
-              </motion.div>
-              {/* Little motion hack to prevent a flash of the exit links when the page loads */}
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={!isLoading ? { opacity: 1 } : { opacity: 0 }}
-                transition={{ duration: 1, delay: 0.5 }}
-              >
-                <ExitLinks
-                  isLoggedIn={isLoggedIn}
-                  isMine={isMine}
-                  className={cn(
-                    '@3xl:hidden w-full items-center justify-center p-6',
-                    isMine && 'mb-20'
-                  )}
-                />
-              </motion.div>
-            </>
-          )}
-        </div>
-
-        {/* Elements which ignore the layout of this container */}
-        <ContactMeDialog
-          open={isContactMeDialogOpen}
-          onOpenChange={setIsContactMeDialogOpen}
-          userId={user.id}
-        />
-        {isMine && (
-          <>
-            <ShareDialog
-              open={isShareDialogOpen}
-              setOpen={setIsShareDialogOpen}
-            />
-            <EditProfileSheet
-              open={isEditing}
-              setOpen={setIsEditing}
+          {/* Left part of the profile. desktop: full height and long enough to render profile details in desktop mode. */}
+          {/* mobile: auto height and short enough to render profile details in mobile mode. */}
+          <div
+            className={cn(
+              '@[81rem]:h-full @[81rem]:min-w-96 flex w-[328px] flex-col justify-between gap-6 p-6',
+              'animate-in fade-in-0 duration-500'
+            )}
+          >
+            <ProfileDetails
               user={user}
+              isMine={isMine}
+              onEdit={() => setIsEditing(true)}
+              onContactMe={() => setIsContactMeDialogOpen(true)}
             />
-            {!showOnboarding && (
-              <div className='fix-modal-layout-shift fixed bottom-0 left-1/2 -translate-x-1/2 -translate-y-6 transform'>
+            <ExitLinks
+              isLoggedIn={isLoggedIn}
+              isMine={isMine}
+              className='@[81rem]:flex hidden'
+            />
+          </div>
+          {/* The right side of the profile. Should handle scroll itself (except on mobile, where the whole page will scroll*/}
+          <div className='@[81rem]:max-w-none @[81rem]:h-full @[81rem]:overflow-y-auto @[81rem]:flex-1 w-full max-w-[895px]'>
+            {showOnboarding ? (
+              <div className='flex h-full w-full items-center justify-center'>
+                <OnboardWithHandles onSubmit={handleAddMultipleWidgets} />
+              </div>
+            ) : (
+              <>
                 <motion.div
-                  initial={{ y: 100, opacity: 0 }}
-                  animate={
-                    !isLoading ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }
-                  }
-                  transition={{
-                    delay: 1,
-                    type: 'spring',
-                    stiffness: 150,
-                    damping: 30,
-                    mass: 2,
-                  }}
+                  initial={{ opacity: 0, y: 10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1, delay: 0.5, ease: 'easeOut' }}
+                  className={cn(!isArtificialMobile && '@[81rem]:mb-20')} // Little hack to make sure control bar doesn't overlap the bottommost widget controls
                 >
-                  <ControlBar
-                    onAddImage={handleAddImage}
-                    onAddLink={handleAddLink}
-                    onShare={() => setIsShareDialogOpen(true)}
-                    isDisabled={isMobile}
+                  <ErrorBoundary FallbackComponent={GridErrorFallback}>
+                    <WidgetGrid immutable={!isMine || disableControlBar} />
+                  </ErrorBoundary>
+                </motion.div>
+                {/* Little motion hack to prevent a flash of the exit links when the page loads */}
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={!isLoading ? { opacity: 1 } : { opacity: 0 }}
+                  transition={{ duration: 1, delay: 0.5 }}
+                >
+                  <ExitLinks
+                    isLoggedIn={isLoggedIn}
+                    isMine={isMine}
+                    className={cn(
+                      '@[81rem]:hidden w-full items-center justify-center p-6',
+                      isMine && !isArtificialMobile && 'mb-20'
+                    )}
                   />
                 </motion.div>
-              </div>
+              </>
             )}
-          </>
-        )}
-      </div>
+          </div>
+          {/* Elements which ignore the layout of this container */}
+          <ContactMeDialog
+            open={isContactMeDialogOpen}
+            onOpenChange={setIsContactMeDialogOpen}
+            userId={user.id}
+          />
+          {isMine && (
+            <>
+              <ShareDialog
+                open={isShareDialogOpen}
+                setOpen={setIsShareDialogOpen}
+              />
+              <EditProfileSheet
+                open={isEditing}
+                setOpen={setIsEditing}
+                user={user}
+              />
+            </>
+          )}
+        </motion.div>
+      </motion.div>
+      {!showOnboarding && (
+        <div className='fix-modal-layout-shift fixed bottom-0 left-1/2 -translate-x-1/2 -translate-y-6 transform'>
+          <motion.div
+            initial={{ y: 100, opacity: 0 }}
+            animate={!isLoading ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
+            transition={{
+              delay: 1,
+              type: 'spring',
+              stiffness: 150,
+              damping: 30,
+              mass: 2,
+            }}
+          >
+            <ControlBar
+              onAddImage={handleAddImage}
+              onAddLink={handleAddLink}
+              onShare={() => setIsShareDialogOpen(true)}
+              isDisabled={disableControlBar}
+              isMobile={isArtificialMobile}
+              onIsMobileChange={
+                disableControlBar ? undefined : setIsArtificialMobile
+              }
+            />
+          </motion.div>
+        </div>
+      )}
     </div>
   );
 };
