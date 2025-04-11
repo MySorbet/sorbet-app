0f26e5b0f5dbfafeec3502499c9e68474150cd7e
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-04-11 00:16:50 -0600

    use size

--- src/app/[handle]/components/mobile-switch/new-feature-badge.tsx
+++ src/app/[handle]/components/mobile-switch/new-feature-badge.tsx
@@ -2,7 +2,7 @@ export const NewFeatureBadge = () => {
   return (
     <div
       title='New'
-      className='bg-sorbet group absolute right-0 top-0 -translate-y-[1px] translate-x-1/4 rounded-full p-1.5'
+      className='bg-sorbet group absolute right-0 top-0 size-3 -translate-y-[1px] translate-x-1/4 rounded-full'
     ></div>
   );
 };
