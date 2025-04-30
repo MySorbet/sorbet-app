33b935375d47f63e2fb0520870c4ad2a0f071aa1
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-04-30 13:35:22 -0600

    Refactor RecipientSheet to include step-based forms for bank and crypto recipients

--- src/app/(with-sidebar)/transfers/components/recipient-sheet.tsx
+++ src/app/(with-sidebar)/transfers/components/recipient-sheet.tsx
@@ -1,3 +1,19 @@
+import { ArrowLeft } from 'lucide-react';
+import { forwardRef, useState } from 'react';
+
+import { BaseAlert } from '@/components/common/base-alert';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { cn } from '@/lib/utils';
+
 import {
   RecipientButton,
   RecipientButtonContent,
@@ -10,6 +26,7 @@ import {
   VaulSheet,
   VaulSheetContent,
   VaulSheetDescription,
+  VaulSheetFooter,
   VaulSheetHeader,
   VaulSheetTitle,
   VaulSheetTrigger,
@@ -17,41 +34,155 @@ import {
 
 /** Render a sheet to walk the user through new recipient creation */
 export const RecipientSheet = () => {
+  const [step, setStep] = useState<'crypto' | 'bank'>();
+
   return (
-    <VaulSheet direction='right'>
+    <VaulSheet onClose={() => setStep(undefined)}>
       <VaulSheetTrigger>Open</VaulSheetTrigger>
-      <VaulSheetContent>
-        <VaulSheetHeader>
-          <VaulSheetTitle>New recipient</VaulSheetTitle>
-          <VaulSheetDescription>
-            Select how you want to transfer funds
-          </VaulSheetDescription>
-        </VaulSheetHeader>
-        <div className='flex flex-col gap-3'>
-          <RecipientButton>
-            <RecipientButtonIcon type='bank' />
-            <RecipientButtonContent>
-              <RecipientButtonTitle>Bank recipient</RecipientButtonTitle>
-              <RecipientButtonDescription>
-                Transfer to a business or individual bank
-              </RecipientButtonDescription>
-              <RecipientButtonDetail>
-                Arrives in 1-2 business days
-              </RecipientButtonDetail>
-            </RecipientButtonContent>
-          </RecipientButton>
-          <RecipientButton>
-            <RecipientButtonIcon type='wallet' />
-            <RecipientButtonContent>
-              <RecipientButtonTitle>Crypto wallet</RecipientButtonTitle>
-              <RecipientButtonDescription>
-                Transfer to crypto wallet or exchange
-              </RecipientButtonDescription>
-              <RecipientButtonDetail>Arrives instantly</RecipientButtonDetail>
-            </RecipientButtonContent>
-          </RecipientButton>
-        </div>
+      <VaulSheetContent className='max-w-sm'>
+        {step ? (
+          step === 'crypto' ? (
+            <CryptoRecipientForm
+              onBack={() => setStep(undefined)}
+              className='animate-in fade-in-0 slide-in-from-right-1 duration-300'
+            />
+          ) : (
+            <BankRecipientForm
+              onBack={() => setStep(undefined)}
+              className='animate-in fade-in-0 slide-in-from-right-1 duration-300'
+            />
+          )
+        ) : (
+          <BankOrCrypto
+            setStep={setStep}
+            className='animate-in fade-in-0 slide-in-from-left-1 duration-300'
+          />
+        )}
       </VaulSheetContent>
     </VaulSheet>
   );
 };
+
+const BankOrCrypto = ({
+  className,
+  setStep,
+}: {
+  className?: string;
+  setStep: (step: 'crypto' | 'bank') => void;
+}) => {
+  return (
+    <>
+      <VaulSheetHeader>
+        {/* For spacing to be consistent */}
+        <BackButton className='opacity-0' aria-hidden />
+        <VaulSheetTitle>New recipient</VaulSheetTitle>
+        <VaulSheetDescription>
+          Select how you want to transfer funds
+        </VaulSheetDescription>
+      </VaulSheetHeader>
+      <div className={cn('flex flex-col gap-3', className)}>
+        <RecipientButton onClick={() => setStep('bank')}>
+          <RecipientButtonIcon type='bank' />
+          <RecipientButtonContent>
+            <RecipientButtonTitle>Bank recipient</RecipientButtonTitle>
+            <RecipientButtonDescription>
+              Transfer to a business or individual bank
+            </RecipientButtonDescription>
+            <RecipientButtonDetail>
+              Arrives in 1-2 business days
+            </RecipientButtonDetail>
+          </RecipientButtonContent>
+        </RecipientButton>
+        <RecipientButton onClick={() => setStep('crypto')}>
+          <RecipientButtonIcon type='wallet' />
+          <RecipientButtonContent>
+            <RecipientButtonTitle>Crypto wallet</RecipientButtonTitle>
+            <RecipientButtonDescription>
+              Transfer to crypto wallet or exchange
+            </RecipientButtonDescription>
+            <RecipientButtonDetail>Arrives instantly</RecipientButtonDetail>
+          </RecipientButtonContent>
+        </RecipientButton>
+      </div>
+    </>
+  );
+};
+
+const BankRecipientForm = ({
+  onBack,
+  className,
+}: {
+  onBack?: () => void;
+  className?: string;
+}) => {
+  return (
+    <>
+      <VaulSheetHeader>
+        <BackButton onClick={onBack} />
+        <VaulSheetTitle>New bank recipient</VaulSheetTitle>
+      </VaulSheetHeader>
+      <div className={cn('flex flex-col gap-3', className)}>
+        <Label>Currency</Label>
+        <Select>
+          <SelectTrigger>
+            <SelectValue placeholder='Select a currency' />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value='usd'>USD</SelectItem>
+            {/* <SelectItem value='eur'>EUR</SelectItem> */}
+          </SelectContent>
+        </Select>
+      </div>
+    </>
+  );
+};
+
+const CryptoRecipientForm = ({
+  onBack,
+  className,
+}: {
+  onBack?: () => void;
+  className?: string;
+}) => {
+  return (
+    <>
+      <VaulSheetHeader>
+        <BackButton onClick={onBack} />
+        <VaulSheetTitle>New crypto recipient</VaulSheetTitle>
+      </VaulSheetHeader>
+      <div className={cn('flex flex-col gap-3', className)}>
+        <BaseAlert
+          title='Is this a Base network address?'
+          description='Make sure this address can accept USDC on Base. If not, you could lose your funds.'
+        />
+        <Label>Label</Label>
+        <Input />
+        <Label>Wallet address</Label>
+        <Input />
+      </div>
+      <VaulSheetFooter className='flex flex-row justify-end'>
+        <Button variant='sorbet' className='w-fit'>
+          Save
+        </Button>
+      </VaulSheetFooter>
+    </>
+  );
+};
+
+const BackButton = forwardRef<
+  HTMLButtonElement,
+  React.ComponentPropsWithoutRef<typeof Button> & { className?: string }
+>(({ className, onClick, ...props }, ref) => {
+  return (
+    <Button
+      ref={ref}
+      variant='ghost'
+      size='icon'
+      className={cn('group -ml-1 size-6', className)}
+      onClick={onClick}
+      {...props}
+    >
+      <ArrowLeft className='transition-transform ease-in-out group-hover:-translate-x-[2px]' />
+    </Button>
+  );
+});
