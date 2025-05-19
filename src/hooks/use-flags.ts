dbc316f43970dd433f3b230a7585fe9e3a048c0d
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-05-19 15:44:34 -0600

    Add more preview users

--- src/hooks/use-flags.ts
+++ src/hooks/use-flags.ts
@@ -1,12 +1,9 @@
 import { useAuth } from '@/hooks';
 import { featureFlags } from '@/lib/flags';
 
-const previewUsers = [
-  'maherayari', // prod
-  'ramioc', // prod
-  'cutaiar', // prod
-];
-const isPreviewUser = (handle: string) => previewUsers.includes(handle);
+/** These are the users who will have access to the preview features shown below in production */
+const PREVIEW_USERS = ['maherayari', 'maher', 'ramioc', 'chrisa', 'cutaiar'];
+const isPreviewUser = (handle: string) => PREVIEW_USERS.includes(handle);
 const isLocal = () => process.env.NODE_ENV === 'development';
 
 /**
