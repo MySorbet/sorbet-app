d45d70b471ce3395e0a3191a06293f66b0a6a4d9
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-09-01 21:41:54 -0700

    Copy updates for page metadata

--- src/app/invoices/layout.tsx
+++ src/app/invoices/layout.tsx
@@ -2,6 +2,8 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Invoices',
+  description:
+    'Simple, secure, and fast payments. Pay invoices globally with near-zero fees.',
 };
 
 /** Noop layout. We just use this file to set metadata */
