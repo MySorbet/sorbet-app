7bcfbd5dc8050333f295aaaa2ad334885cb5c3e9
Dillon Cutaiar <dmcutaiar@gmail.com>
2025-09-02 22:30:54 -0700

    Add a glyph component for new svg assets

---
+++ src/components/common/glyph/glyph.tsx
@@ -0,0 +1,34 @@
+import { cn } from '@/lib/utils';
+
+import arrow from './svg/arrow.svg';
+import asterisk from './svg/asterisk.svg';
+import diamond from './svg/diamond.svg';
+import petal from './svg/petal.svg';
+import square from './svg/square.svg';
+import wink from './svg/wink.svg';
+
+export const glyphs: Record<string, React.FC<React.SVGProps<SVGSVGElement>>> = {
+  asterisk: asterisk,
+  wink: wink,
+  petal: petal,
+  arrow: arrow,
+  square: square,
+  diamond: diamond,
+};
+
+type GlyphType = keyof typeof glyphs;
+
+export const Glyph = ({
+  type,
+  className,
+}: {
+  type?: GlyphType;
+  className?: string;
+}) => {
+  const GlyphComponent = glyphs[type || 'asterisk'];
+  return (
+    <div>
+      <GlyphComponent className={cn('size-10', className)} />
+    </div>
+  );
+};
